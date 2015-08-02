8fa9af27767fb647bb5cb5fae619d1bf12da7cee
abstractspoon <daniel.godson@gmail.com>
2015-08-02 19:53:42 +1000

    Change indents. Add comments

--- ImportExport/ExporterBridge/ExporterBridge.cpp
+++ ImportExport/ExporterBridge/ExporterBridge.cpp
@@ -35,43 +35,44 @@ CExporterBridge::CExporterBridge()
 
 void CExporterBridge::Release()
 {
-   delete this;
+	delete this;
 }
 
 void CExporterBridge::SetLocalizer(ITransText* /*pTT*/)
 {
-
+	// TODO
 }
 
 LPCTSTR CExporterBridge::GetMenuText() const
 {
-   return L"Bridge";
+	return L"Bridge";
 }
 
 LPCTSTR CExporterBridge::GetFileFilter() const
 {
-   return L"zzz";
+	return L"zzz";
 }
 
 LPCTSTR CExporterBridge::GetFileExtension() const
 {
-   return L"zzz";
+	return L"zzz";
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 bool CExporterBridge::Export(const ITaskList* pSrcTaskFile, LPCTSTR szDestFilePath, BOOL bSilent, IPreferences* pPrefs, LPCTSTR szKey)
 {
-   // call into out sibling C# module to do the actual work
-   msclr::auto_gcroot<Class1^> expCore = gcnew Class1();
-   msclr::auto_gcroot<CPreferences^> prefs = gcnew CPreferences(pPrefs);
-   msclr::auto_gcroot<CTaskList^> srcTasks = gcnew CTaskList(GetITLInterface<ITaskList14>(pSrcTaskFile, IID_TASKLIST14));
-      
-   // do the export
-   return expCore->Export(srcTasks.get(), gcnew String(szDestFilePath), (bSilent != FALSE), prefs.get(), gcnew String(szKey));
+	// call into out sibling C# module to do the actual work
+	msclr::auto_gcroot<Class1^> expCore = gcnew Class1();
+	msclr::auto_gcroot<CPreferences^> prefs = gcnew CPreferences(pPrefs);
+	msclr::auto_gcroot<CTaskList^> srcTasks = gcnew CTaskList(GetITLInterface<ITaskList14>(pSrcTaskFile, IID_TASKLIST14));
+	
+	// do the export
+	return expCore->Export(srcTasks.get(), gcnew String(szDestFilePath), (bSilent != FALSE), prefs.get(), gcnew String(szKey));
 }
 
 bool CExporterBridge::Export(const IMultiTaskList* pSrcTaskFile, LPCTSTR szDestFilePath, BOOL bSilent, IPreferences* pPrefs, LPCTSTR szKey)
 {
-   return false;
+	// TODO
+	return false;
 }
