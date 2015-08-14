6f9a75d75a739a86dbfa17bade82575e28d0168c
abstractspoon <daniel.godson@MSC044.maptek-au.local>
2015-08-14 17:04:53 +1000

    Begin to incorporate MarkdownLog

--- ImportExport/MarkdownImpExp/MarkdownImpExpCore/MarkdownImpExpCore.cs
+++ ImportExport/MarkdownImpExp/MarkdownImpExpCore/MarkdownImpExpCore.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 using System.Windows.Forms;
 using PluginHelpers;
 using MarkdownLog;
@@ -13,18 +14,46 @@ namespace MarkdownImpExp
         public bool Export(CTaskList srcTasks, string sDestFilePath, bool bSilent, CPreferences prefs, string sKey)
         {
             UInt32 taskCount = srcTasks.GetTaskCount();
+
+            MarkdownContainer mdTasks = new MarkdownContainer();
+
             IntPtr hTask = srcTasks.GetFirstTask(IntPtr.Zero);
 
             while (hTask != IntPtr.Zero)
             {
-                String sTitle = srcTasks.GetTaskTitle(hTask);
-
-
-
-
+                ExportTask(srcTasks, hTask, mdTasks);
 
                 hTask = srcTasks.GetNextTask(hTask);
             }
+
+            Debug.Write(mdTasks.ToMarkdown());
+
+            return true;
+        }
+
+        public bool ExportTask(CTaskList srcTasks, IntPtr hTask, MarkdownContainer mdParent)
+        {
+            // each task is a container, containing itself 
+            // and a bulleted list of subtasks
+            MarkdownContainer mdTask = new MarkdownContainer();
+
+            // add ourselves
+            String sTitle = srcTasks.GetTaskTitle(hTask);
+            mdTask.Append(new Paragraph(sTitle));
+
+            // then our subtasks as a bulleted list
+            BulletedList subtasks = new BulletedList();
+
+            IntPtr hSubTask = srcTasks.GetFirstTask(hTask);
+
+            while (hSubTask != IntPtr.Zero)
+            {
+                ExportTask(srcTasks, hSubTask, mdTask);
+
+                hSubTask = srcTasks.GetNextTask(hSubTask);
+            }
+
+            mdParent.Append(mdTask);
 
             return true;
         }
