f72fa6c948452bb5b000f5ea0a1b8d72f94ccd58
abstractspoon <daniel.godson@gmail.com>
2016-07-05 22:16:40 +1000

    Restore files

---
+++ PluginHelpers/TDLContentControl.cpp
@@ -0,0 +1,35 @@
+// PluginHelpers.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "pluginhelpers.h"
+#include "TDLContentControl.h"
+
+#include "..\..\Interfaces\UITheme.h"
+#include "..\..\Interfaces\IContentControl.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+using namespace TDLPluginHelpers;
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+TDLContentControl::TDLNotify::TDLNotify(IntPtr hwndParent) : m_hwndParent(NULL), m_hwndFrom(NULL)
+{
+	m_hwndParent = static_cast<HWND>(hwndParent.ToPointer());
+}
+
+TDLContentControl::TDLNotify::TDLNotify(IntPtr hwndParent, IntPtr hwndFrom) : m_hwndParent(NULL), m_hwndFrom(NULL)
+{
+	m_hwndParent = static_cast<HWND>(hwndParent.ToPointer());
+	m_hwndFrom = static_cast<HWND>(hwndFrom.ToPointer());
+}
+
+bool TDLContentControl::TDLNotify::NotifyMod()
+{
+	if (!IsWindow(m_hwndParent))
+		return false;
+
+	::SendMessage(m_hwndParent, WM_ICC_COMMENTSCHANGE, 0, 0);
+	return true;
+}
