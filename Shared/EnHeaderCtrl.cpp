d860efe48cc835b3f1fe99c765575dfc7a979abe
abstractspoon <daniel.godson@gmail.com>
2017-05-12 23:51:47 +1000

    Fix compile warning

--- Shared/EnHeaderCtrl.cpp
+++ Shared/EnHeaderCtrl.cpp
@@ -906,7 +906,7 @@ int CEnHeaderCtrl::OnToolHitTest(CPoint point, TOOLINFO* pTI) const
 	if ((nItem != -1) && IsItemVisible(nItem))
 	{
 #ifdef _DEBUG
-		if (m_tooltips.GetLastHitToolInfo().uId != nItem)
+		if (m_tooltips.GetLastHitToolInfo().uId != (UINT)nItem)
 			TRACE(_T("CEnHeaderCtrl::OnToolHitTest(%d, %s)\n"), nItem, GetItemToolTip(nItem));
 #endif
 
