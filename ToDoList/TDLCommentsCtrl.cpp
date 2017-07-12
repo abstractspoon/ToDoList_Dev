53159b11c382ae155eb07ac460e9a21f0e38061a
dan.g <daniel.godson@gmail.com>
2017-07-12 18:25:49 +1000

    Add placeholder for encapsulated comment control

---
+++ ToDoList/TDLCommentsCtrl.cpp
@@ -0,0 +1,32 @@
+// TDLCommentsCtrl.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "ToDoList.h"
+#include "TDLCommentsCtrl.h"
+
+
+// CTDLCommentsCtrl dialog
+
+IMPLEMENT_DYNAMIC(CTDLCommentsCtrl, CDialog)
+
+CTDLCommentsCtrl::CTDLCommentsCtrl()
+{
+
+}
+
+CTDLCommentsCtrl::~CTDLCommentsCtrl()
+{
+}
+
+void CTDLCommentsCtrl::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CTDLCommentsCtrl, CDialog)
+END_MESSAGE_MAP()
+
+
+// CTDLCommentsCtrl message handlers
---
+++ ToDoList/TDLCommentsCtrl.h
@@ -0,0 +1,30 @@
+#if !defined(AFX_TDLCOMMENTCTRL_H__852964E3_4ABD_4B66_88BA_F553177616F2__INCLUDED_)
+#define AFX_TDLCOMMENTCTRL_H__852964E3_4ABD_4B66_88BA_F553177616F2__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+/////////////////////////////////////////////////////////////////////////////
+// PreferencesTaskDefPage.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+
+class CTDLCommentsCtrl : public CDialog
+{
+	DECLARE_DYNAMIC(CTDLCommentsCtrl)
+
+public:
+	CTDLCommentsCtrl();
+	virtual ~CTDLCommentsCtrl();
+
+protected:
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+};
+
+#endif // AFX_TDLCOMMENTCTRL_H__852964E3_4ABD_4B66_88BA_F553177616F2__INCLUDED_
