6339d2ddbd068f0bb60f1e3a18b0d83f5222072c
abstractspoon <daniel.godson@gmail.com>
2017-08-22 13:42:19 +1000

    Refactor to remove duplicated code

--- ToDoList/TDLColumnAttribVisibilityListCtrl.cpp
+++ ToDoList/TDLColumnAttribVisibilityListCtrl.cpp
@@ -282,24 +282,8 @@ COLORREF CTDLColumnAttribVisibilityListCtrl::GetItemBackColor(int nItem, int nCo
 {
 	if (!bSelected)
 	{
-		if (!IsWindowEnabled())
+		if (!IsWindowEnabled() || !IsButtonEnabled(nItem, nCol))
 			return GetSysColor(COLOR_3DLIGHT);
-
-		if (!IsTimeCellEnabled(nItem, nCol))
-			return GetSysColor(COLOR_3DLIGHT);
-		
-		switch (nCol)
-		{
-		case COL_EDITVIS:
-			if (m_vis.GetShowFields() != TDLSA_ANY)
-				return GetSysColor(COLOR_3DLIGHT);
-			break;
-
-		case COL_FILTERVIS:
-			if (m_vis.GetShowFields() != TDLSA_ANY)
-				return GetSysColor(COLOR_3DLIGHT);
-			break;
-		}
 	}
 	
 	// else
