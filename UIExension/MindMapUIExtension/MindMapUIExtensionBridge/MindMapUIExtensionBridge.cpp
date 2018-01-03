08a950d83ff40015f51a6d46f693bb9f340b5b56
abstractspoon <daniel.godson@gmail.com>
2018-01-03 12:55:48 +1100

    Add Mind Map plugin

---
+++ UIExension/MindMapUIExtension/MindMapUIExtensionBridge/MindMapUIExtensionBridge.cpp
@@ -0,0 +1,265 @@
+// ExporterBridge.cpp : Defines the exported functions for the DLL application.
+//
+
+#include <unknwn.h>
+#include <tchar.h>
+#include <msclr\auto_gcroot.h>
+
+#include "stdafx.h"
+#include "resource.h"
+#include "MindMapUIExtensionBridge.h"
+
+#include "..\..\..\..\ToDoList_Dev\Interfaces\ITasklist.h"
+#include "..\..\..\..\ToDoList_Dev\Interfaces\ITransText.h"
+#include "..\..\..\..\ToDoList_Dev\Interfaces\IPreferences.h"
+#include "..\..\..\..\ToDoList_Dev\Interfaces\UITheme.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef _DEBUG
+#	using <..\..\..\Debug\MindMapUIExtensionCore.dll>
+#	using <..\..\..\Debug\PluginHelpers.dll> as_friend
+#else
+#	using <..\..\..\Release\MindMapUIExtensionCore.dll>
+#	using <..\..\..\Release\PluginHelpers.dll> as_friend
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+using namespace MindMapUIExtension;
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Runtime::InteropServices;
+using namespace Abstractspoon::Tdl::PluginHelpers;
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// REPLACE THIS WITH NEW GUID!
+const LPCWSTR MINDMAP_GUID = L"14FC02B5-2DE0-4A2F-BA6A-50A66551864E";
+const LPCWSTR MINDMAP_NAME = L"Mind Map";
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+CMindMapUIExtensionBridge::CMindMapUIExtensionBridge() : m_hIcon(NULL), m_pTT(nullptr)
+{
+	HMODULE hMod = LoadLibrary(L"MindMapUIExtensionBridge.dll"); // us
+
+	m_hIcon = ::LoadIcon(hMod, MAKEINTRESOURCE(IDI_MINDMAP));
+}
+
+void CMindMapUIExtensionBridge::Release()
+{
+	delete this;
+}
+
+void CMindMapUIExtensionBridge::SetLocalizer(ITransText* pTT)
+{
+	if (m_pTT == nullptr)
+		m_pTT = pTT;
+}
+
+LPCWSTR CMindMapUIExtensionBridge::GetMenuText() const
+{
+	return MINDMAP_NAME;
+}
+
+HICON CMindMapUIExtensionBridge::GetIcon() const
+{
+	return m_hIcon;
+}
+
+LPCWSTR CMindMapUIExtensionBridge::GetTypeID() const
+{
+	return MINDMAP_GUID;
+}
+
+IUIExtensionWindow* CMindMapUIExtensionBridge::CreateExtWindow(UINT nCtrlID, 
+	DWORD nStyle, long nLeft, long nTop, long nWidth, long nHeight, HWND hwndParent)
+{
+	CMindMapUIExtensionBridgeWindow* pExtWnd = new CMindMapUIExtensionBridgeWindow;
+
+	if (!pExtWnd->Create(nCtrlID, nStyle, nLeft, nTop, nWidth, nHeight, hwndParent))
+	{
+		pExtWnd->Release();
+		pExtWnd = NULL;
+	}
+
+	return pExtWnd;
+}
+
+void CMindMapUIExtensionBridge::SavePreferences(IPreferences* pPrefs, LPCWSTR szKey) const
+{
+	// TODO
+}
+
+void CMindMapUIExtensionBridge::LoadPreferences(const IPreferences* pPrefs, LPCWSTR szKey)
+{
+	// TODO
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+CMindMapUIExtensionBridgeWindow::CMindMapUIExtensionBridgeWindow()
+{
+
+}
+
+void CMindMapUIExtensionBridgeWindow::Release()
+{
+	::DestroyWindow(GetHwnd());
+	delete this;
+}
+
+BOOL CMindMapUIExtensionBridgeWindow::Create(UINT nCtrlID, DWORD nStyle, 
+	long nLeft, long nTop, long nWidth, long nHeight, HWND hwndParent)
+{
+	m_source = gcnew System::Windows::Interop::HwndSource(
+		CS_VREDRAW | CS_HREDRAW,
+		nStyle,
+		0,
+		nLeft,
+		nTop,
+		nWidth,
+		nHeight,
+		"",
+		System::IntPtr(hwndParent));
+
+	if (m_source->Handle != IntPtr::Zero)
+	{
+		m_wnd = gcnew MindMapUIExtension::MindMapUIExtensionCore();
+		m_source->RootVisual = m_wnd;
+
+		return true;
+	}
+
+	return false;
+}
+
+HICON CMindMapUIExtensionBridgeWindow::GetIcon() const
+{
+	return NULL;
+}
+
+LPCWSTR CMindMapUIExtensionBridgeWindow::GetMenuText() const
+{
+	return MINDMAP_NAME;
+}
+
+LPCWSTR CMindMapUIExtensionBridgeWindow::GetTypeID() const
+{
+	return MINDMAP_GUID;
+}
+
+bool CMindMapUIExtensionBridgeWindow::SelectTask(DWORD dwTaskID)
+{
+	return m_wnd->SelectTask(dwTaskID);
+}
+
+bool CMindMapUIExtensionBridgeWindow::SelectTasks(const DWORD* pdwTaskIDs, int nTaskCount)
+{
+	array<UInt32>^ taskIDs = gcnew array<UInt32>(nTaskCount);
+
+	for (int i = 0; i < nTaskCount; i++)
+		taskIDs[i] = pdwTaskIDs[i];
+
+	return m_wnd->SelectTasks(taskIDs);
+}
+
+void CMindMapUIExtensionBridgeWindow::UpdateTasks(const ITaskList* pTasks, IUI_UPDATETYPE nUpdate, const IUI_ATTRIBUTE* pAttributes, int nNumAttributes)
+{
+	msclr::auto_gcroot<TaskList^> tasks = gcnew TaskList(pTasks);
+
+	m_wnd->UpdateTasks(tasks.get(), UIExtension::Map(nUpdate), UIExtension::Map(pAttributes, nNumAttributes));
+}
+
+bool CMindMapUIExtensionBridgeWindow::WantEditUpdate(IUI_ATTRIBUTE nAttribute) const
+{
+	return m_wnd->WantEditUpdate(UIExtension::Map(nAttribute));
+}
+
+bool CMindMapUIExtensionBridgeWindow::WantSortUpdate(IUI_ATTRIBUTE nAttribute) const
+{
+	return m_wnd->WantSortUpdate(UIExtension::Map(nAttribute));
+}
+
+bool CMindMapUIExtensionBridgeWindow::PrepareNewTask(ITaskList* pTask) const
+{
+	msclr::auto_gcroot<TaskList^> task = gcnew TaskList(pTask);
+
+	return m_wnd->PrepareNewTask(task.get()->GetFirstTask());
+}
+
+bool CMindMapUIExtensionBridgeWindow::ProcessMessage(MSG* pMsg)
+{
+	return m_wnd->ProcessMessage(IntPtr(pMsg->hwnd), 
+										pMsg->message, 
+										pMsg->wParam, 
+										pMsg->lParam, 
+										pMsg->time, 
+										pMsg->pt.x,
+										pMsg->pt.y);
+}
+
+bool CMindMapUIExtensionBridgeWindow::DoAppCommand(IUI_APPCOMMAND nCmd, DWORD dwExtra)
+{
+// 	switch (nCmd)
+// 	{
+// 		// TODO
+// 	}
+
+	return false;
+}
+
+bool CMindMapUIExtensionBridgeWindow::CanDoAppCommand(IUI_APPCOMMAND nCmd, DWORD dwExtra) const
+{
+// 	switch (nCmd)
+// 	{
+// 		// TODO
+// 	}
+
+	return false;
+}
+
+bool CMindMapUIExtensionBridgeWindow::GetLabelEditRect(LPRECT pEdit)
+{
+	return m_wnd->GetLabelEditRect((Int32&)pEdit->left, (Int32&)pEdit->top, (Int32&)pEdit->right, (Int32&)pEdit->bottom);
+}
+
+IUI_HITTEST CMindMapUIExtensionBridgeWindow::HitTest(const POINT& ptScreen) const
+{
+	return UIExtension::Map(m_wnd->HitTest(ptScreen.x, ptScreen.y));
+}
+
+void CMindMapUIExtensionBridgeWindow::SetUITheme(const UITHEME* pTheme)
+{
+	msclr::auto_gcroot<UITheme^> theme = gcnew UITheme(pTheme);
+
+	m_wnd->SetUITheme(theme.get());
+}
+
+void CMindMapUIExtensionBridgeWindow::SetReadOnly(bool bReadOnly)
+{
+	m_wnd->SetReadOnly(bReadOnly);
+}
+
+HWND CMindMapUIExtensionBridgeWindow::GetHwnd() const
+{
+	return static_cast<HWND>(m_source->Handle.ToPointer());
+}
+
+void CMindMapUIExtensionBridgeWindow::SavePreferences(IPreferences* pPrefs, LPCWSTR szKey) const
+{
+	msclr::auto_gcroot<Preferences^> prefs = gcnew Preferences(pPrefs);
+	msclr::auto_gcroot<String^> key = gcnew String(szKey);
+
+	m_wnd->SavePreferences(prefs.get(), key.get());
+}
+
+void CMindMapUIExtensionBridgeWindow::LoadPreferences(const IPreferences* pPrefs, LPCWSTR szKey, bool bAppOnly)
+{
+	msclr::auto_gcroot<Preferences^> prefs = gcnew Preferences(pPrefs);
+	msclr::auto_gcroot<String^> key = gcnew String(szKey);
+
+	m_wnd->LoadPreferences(prefs.get(), key.get(), bAppOnly);
+}
+
---
+++ UIExension/MindMapUIExtension/MindMapUIExtensionCore/MindMapUIExtensionCore.cs
@@ -0,0 +1,365 @@
+﻿
+using System;
+using Abstractspoon.Tdl.PluginHelpers;
+
+// PLS DON'T ADD 'USING' STATEMENTS WHILE I AM STILL LEARNING!
+
+namespace MindMapUIExtension
+{
+    public class SampleListItem
+    {
+        public String Type { get; set; }
+        public String Attrib { get; set; }
+        public String Value { get; set; }
+        public String Tasks { get; set; }
+    }
+
+    public class MindMapUIExtensionCore : System.Windows.Controls.Grid, IUIExtension
+    {
+        public MindMapUIExtensionCore()
+        {
+            InitializeComponent();
+        }
+
+        // IUIExtension ------------------------------------------------------------------
+
+        public bool SelectTask(UInt32 dwTaskID)
+        {
+            SampleListItem item = new SampleListItem();
+
+            item.Value = dwTaskID.ToString();
+            item.Type = "Selection";
+
+            m_Items.Add(item);
+            m_ListView.Items.Refresh();
+
+            return true;
+        }
+
+	    public bool SelectTasks(UInt32[] pdwTaskIDs)
+        {
+            return false;
+        }
+
+	    public void UpdateTasks(TaskList tasks, 
+                                UIExtension.UpdateType type,
+                                System.Collections.Generic.HashSet<UIExtension.TaskAttribute> attribs)
+        {
+            Task task = tasks.GetFirstTask();
+            SampleListItem item = new SampleListItem();
+
+            System.Collections.Generic.List<String> attrib = new System.Collections.Generic.List<String>();
+            System.Collections.Generic.List<String> value = new System.Collections.Generic.List<String>();
+            
+            if (attribs.Contains(UIExtension.TaskAttribute.Title))
+            {
+                attrib.Add("Title");
+                value.Add(task.GetTitle());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.DoneDate))
+            {
+                attrib.Add("Done Date");
+                value.Add(task.GetDoneDateString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.DueDate))
+            {
+                attrib.Add("Due Date");
+                value.Add(task.GetDueDateString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.StartDate))
+            {
+                attrib.Add("Start Date");
+                value.Add(task.GetStartDateString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Priority))
+            {
+                attrib.Add("Priority");
+                value.Add(task.GetPriority().ToString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Color))
+            {
+                attrib.Add("Color");
+                value.Add(task.GetColor().ToString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.AllocTo))
+            {
+                attrib.Add("Alloc To");
+                value.Add(String.Join(", ", task.GetAllocatedTo()));
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.AllocBy))
+            {
+                attrib.Add("Alloc By");
+                value.Add(task.GetAllocatedBy());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Status))
+            {
+                attrib.Add("Status");
+                value.Add(task.GetStatus());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Category))
+            {
+                attrib.Add("Category");
+                value.Add(String.Join(", ", task.GetCategory()));
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Percent))
+            {
+                attrib.Add("Percent");
+                value.Add(task.GetPercentDone().ToString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.TimeEstimate))
+            {
+                attrib.Add("Time Estimate");
+
+                Task.TimeUnits units = Task.TimeUnits.Hours;
+                value.Add(task.GetTimeEstimate(ref units).ToString() + units);
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.TimeSpent))
+            {
+                attrib.Add("Time Spent");
+
+                Task.TimeUnits units = Task.TimeUnits.Hours;
+                value.Add(task.GetTimeSpent(ref units).ToString() + units);
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.FileReference))
+            {
+                attrib.Add("File Reference");
+                value.Add(String.Join(", ", task.GetFileReference()));
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Comments))
+            {
+                attrib.Add("Comments");
+                value.Add(task.GetComments());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Flag))
+            {
+                attrib.Add("Flag");
+                value.Add(task.IsFlagged().ToString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.CreationDate))
+            {
+                attrib.Add("Creation Date");
+                value.Add(task.GetCreationDateString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.CreatedBy))
+            {
+                attrib.Add("Created By");
+                value.Add(task.GetCreatedBy());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Risk))
+            {
+                attrib.Add("Risk");
+                value.Add(task.GetRisk().ToString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.ExternalId))
+            {
+                attrib.Add("External ID");
+                value.Add(task.GetExternalID());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Cost))
+            {
+                attrib.Add("Cost");
+                value.Add(task.GetCost().ToString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Dependency))
+            {
+                attrib.Add("Dependency");
+                value.Add(String.Join(", ", task.GetDependency()));
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Recurrence))
+            {
+                attrib.Add("Recurrence");
+                //value.Add(task.GetRecurrence());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Version))
+            {
+                attrib.Add("Version");
+                value.Add(task.GetVersion());
+            }
+
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Position))
+            {
+                attrib.Add("Position");
+                value.Add(task.GetPositionString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Id))
+            {
+                attrib.Add("ID");
+                value.Add(task.GetID().ToString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.LastModified))
+            {
+                attrib.Add("Last Modified");
+                //value.Add(task.GetLastModifiedString());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Icon))
+            {
+                attrib.Add("Icon");
+                value.Add(task.GetIcon());
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.Tag))
+            {
+                attrib.Add("Tag");
+                value.Add(String.Join(", ", task.GetTag()));
+            }
+
+            if (attribs.Contains(UIExtension.TaskAttribute.CustomAttribute))
+            {
+                attrib.Add("Custom Attribute");
+                //value.Add(task.GetCustomAttributeData());
+            }
+
+            item.Attrib = String.Join(",", attrib.ToArray());
+            item.Value = String.Join(",", value.ToArray());
+            item.Tasks = task.GetID().ToString();
+
+            switch (type)
+            {
+                case UIExtension.UpdateType.Edit: item.Type = "Edit"; break;
+                case UIExtension.UpdateType.New: item.Type = "Add Task"; break;
+                case UIExtension.UpdateType.Delete: item.Type = "Move Task(s)"; break;
+                case UIExtension.UpdateType.Move: item.Type = "Delete Task(s)"; break;
+                case UIExtension.UpdateType.All: item.Type = "All"; break;
+                case UIExtension.UpdateType.Unknown: item.Type = "Unknown"; break;
+            }
+
+            m_Items.Add(item);
+            m_ListView.Items.Refresh();
+        }
+
+        public bool WantEditUpdate(UIExtension.TaskAttribute attrib)
+	    {
+			return true; // all updates
+	    }
+
+        public bool WantSortUpdate(UIExtension.TaskAttribute attrib)
+        {
+			return true; // all updates
+        }
+	   
+        public bool PrepareNewTask(ref Task task)
+	    {
+		    return false;
+    	}
+
+        public bool ProcessMessage(IntPtr hwnd, UInt32 message, UInt32 wParam, UInt32 lParam, UInt32 time, Int32 xPos, Int32 yPos)
+	    {
+		    return false;
+	    }
+	   
+	    public bool GetLabelEditRect(ref Int32 left, ref Int32 top, ref Int32 right, ref Int32 bottom)
+	    {
+			return false;
+	    }
+
+        public UIExtension.HitResult HitTest(Int32 xPos, Int32 yPos)
+	    {
+            return UIExtension.HitResult.Nowhere;
+	    }
+
+        public void SetUITheme(UITheme theme)
+        {
+            System.Windows.Media.Color bkColor = theme.GetAppMediaColor(UITheme.AppColor.AppBackDark);
+
+            this.Background = new System.Windows.Media.SolidColorBrush(bkColor);
+        }
+
+        public void SetReadOnly(bool bReadOnly)
+	    {
+	    }
+
+	    public void SavePreferences(Preferences prefs, String key)
+	    {
+	    }
+
+	    public void LoadPreferences(Preferences prefs, String key, bool appOnly)
+	    {
+	    }
+        
+        // PRIVATE ------------------------------------------------------------------------------
+
+        private void InitializeComponent()
+        {
+            this.Background = System.Windows.Media.Brushes.White;
+
+            CreateListView();
+            PopulateListView();
+        }
+
+        private void CreateListView()
+        {
+            m_ListView = new System.Windows.Controls.ListView();
+            m_GridView = new System.Windows.Controls.GridView();
+
+            m_TypeCol = new System.Windows.Controls.GridViewColumn();
+            m_TypeCol.DisplayMemberBinding = new System.Windows.Data.Binding("Type");
+            m_TypeCol.Header = "Change Type";
+            m_TypeCol.Width = 200;
+            m_GridView.Columns.Add(m_TypeCol);
+
+            m_AttribCol = new System.Windows.Controls.GridViewColumn();
+            m_AttribCol.DisplayMemberBinding = new System.Windows.Data.Binding("Attrib");
+            m_AttribCol.Header = "Attribute Changed";
+            m_AttribCol.Width = 200;
+            m_GridView.Columns.Add(m_AttribCol);
+
+            m_ValueCol = new System.Windows.Controls.GridViewColumn();
+            m_ValueCol.DisplayMemberBinding = new System.Windows.Data.Binding("Value");
+            m_ValueCol.Header = "New Value";
+            m_ValueCol.Width = 200;
+            m_GridView.Columns.Add(m_ValueCol);
+
+            m_TasksCol = new System.Windows.Controls.GridViewColumn();
+            m_TasksCol.DisplayMemberBinding = new System.Windows.Data.Binding("Tasks");
+            m_TasksCol.Header = "Tasks Changed";
+            m_TasksCol.Width = 200;
+            m_GridView.Columns.Add(m_TasksCol);
+
+            m_ListView.View = m_GridView;
+
+            this.Children.Add(m_ListView);
+        }
+
+        private void PopulateListView()
+        {
+            m_Items = new System.Collections.Generic.List<SampleListItem>();
+            m_ListView.ItemsSource = m_Items;
+        }
+
+        // --------------------------------------------------------------------------------------
+        private System.Collections.Generic.List<SampleListItem> m_Items;
+        private System.Windows.Controls.ListView m_ListView;
+        private System.Windows.Controls.GridView m_GridView;
+        private System.Windows.Controls.GridViewColumn m_TypeCol;
+        private System.Windows.Controls.GridViewColumn m_AttribCol;
+        private System.Windows.Controls.GridViewColumn m_ValueCol;
+        private System.Windows.Controls.GridViewColumn m_TasksCol;
+    }
+}
