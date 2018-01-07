5a08f012e707f207648c8c8212240b98506ad469
abstractspoon <daniel.godson@gmail.com>
2018-01-08 10:55:25 +1100

    Fix compile error

--- UIExension/SampleUIExtension/SampleUIExtensionCore/SampleUIExtensionCore.cs
+++ UIExension/SampleUIExtension/SampleUIExtensionCore/SampleUIExtensionCore.cs
@@ -245,7 +245,6 @@ namespace SampleUIExtension
                 case UIExtension.UpdateType.Edit: item.Type = "Edit"; break;
                 case UIExtension.UpdateType.New: item.Type = "Add Task"; break;
                 case UIExtension.UpdateType.Delete: item.Type = "Move Task(s)"; break;
-                case UIExtension.UpdateType.Move: item.Type = "Delete Task(s)"; break;
                 case UIExtension.UpdateType.All: item.Type = "All"; break;
                 case UIExtension.UpdateType.Unknown: item.Type = "Unknown"; break;
             }
