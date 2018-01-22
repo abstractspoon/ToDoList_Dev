7eb80762434cb6b9b401d16c4906b07f2a4189f1
abstractspoon <daniel.godson@gmail.com>
2018-01-22 15:57:01 +1100

    Add framework for preferences dialog

---
+++ UIExension/DayViewUIExtension/DayViewUIExtensionCore/DayViewPreferences.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DayViewUIExtension
+{
+    public partial class DayViewPreferences : Form
+    {
+        public DayViewPreferences()
+        {
+            InitializeComponent();
+        }
+    }
+}
