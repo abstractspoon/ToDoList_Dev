a497d1cb333bcd5a36e09ef0dd99637284a7a1c6
abstractspoon <daniel.godson@gmail.com>
2018-01-29 10:33:31 +1100

    Also copy resources to plugin build folders

--- CopyResToBuildFolders.bat
+++ CopyResToBuildFolders.bat
@@ -12,3 +12,9 @@ REM - Copy Resources to latest release
 xcopy .\ToDoList_Resources\*.* .\ToDoList_%1\ToDoList\Unicode_Debug\Resources\   /E /EXCLUDE:.\ToDoList_Dev\CopyResToBuildFolders_Exclude.txt /Y 
 
 xcopy .\ToDoList_Resources\*.* .\ToDoList_%1\ToDoList\Unicode_Release\Resources\ /E /EXCLUDE:.\ToDoList_Dev\CopyResToBuildFolders_Exclude.txt /Y 
+
+REM - Copy Resources to Plugins
+
+xcopy .\ToDoList_Resources\*.* .\ToDoList_Plugins\Debug\Resources\   /E /EXCLUDE:.\ToDoList_Dev\CopyResToBuildFolders_Exclude.txt /Y 
+
+xcopy .\ToDoList_Resources\*.* .\ToDoList_Plugins\Release\Resources\ /E /EXCLUDE:.\ToDoList_Dev\CopyResToBuildFolders_Exclude.txt /Y 
