bc7618ce9d23c69e4f3eecfd5c5436184b64b48a
dan.g <daniel.godson@gmail.com>
2018-05-09 22:49:34 +1000

    Fix compile warning

--- GanttChartExt/GanttChartWnd.cpp
+++ GanttChartExt/GanttChartWnd.cpp
@@ -667,10 +667,10 @@ bool CGanttChartWnd::CanDoAppCommand(IUI_APPCOMMAND nCmd, const IUIAPPCOMMANDDAT
 	switch (nCmd)
 	{
 	case IUI_EXPANDALL:
-		return m_tree.TCH().IsAnyItemCollapsed();
+		return (m_tree.TCH().IsAnyItemCollapsed() != FALSE);
 
 	case IUI_COLLAPSEALL:
-		return m_tree.TCH().IsAnyItemExpanded();
+		return (m_tree.TCH().IsAnyItemExpanded() != FALSE);
 
 	case IUI_RESIZEATTRIBCOLUMNS:
 	case IUI_SELECTTASK:
