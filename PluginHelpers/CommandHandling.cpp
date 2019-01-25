6b318eab905f6b17e1bc99dcad20f75f0d434395
dan.g <abstractspoon@users.noreply.github.com>
2019-01-25 15:29:31 +1100

    More useful helper methods

--- PluginHelpers/CommandHandling.cpp
+++ PluginHelpers/CommandHandling.cpp
@@ -15,29 +15,14 @@ using namespace Windows::Forms;
 
 bool CommandHandling::HideCommand(String^ commandId, ToolStripItemCollection^ items)
 {
-	int nItem = items->Count;
+	ToolStripItem^ cmd = GetCommandItem(commandId, items);
 
-	while (nItem--)
+	if (cmd != nullptr)
 	{
-		ToolStripItem^ cmd = items[nItem];
+		cmd->Visible = false;
+		cmd->Enabled = false;
 
-		if (cmd->Name == commandId)
-		{
-			cmd->Visible = false;
-			cmd->Enabled = false;
-
-			return true;
-		}
-		else
-		{
-			ToolStripMenuItem^ menu = dynamic_cast<ToolStripMenuItem^>(cmd);
-
-			if ((menu != nullptr) && menu->HasDropDownItems)
-			{
-				if (HideCommand(commandId, menu->DropDownItems))
-					return true;
-			}
-		}
+		return true;
 	}
 
 	return false;
@@ -45,29 +30,104 @@ bool CommandHandling::HideCommand(String^ commandId, ToolStripItemCollection^ it
 
 bool CommandHandling::ProcessMenuShortcut(Keys keyPress, ToolStripItemCollection^ items)
 {
+	ToolStripMenuItem^ menu = GetMenuItem(keyPress, items);
+
+	if (menu != nullptr)
+	{
+		if (menu->Enabled)
+			menu->PerformClick();
+
+		return true;
+	}
+
+	return false;
+}
+
+System::Windows::Forms::Keys CommandHandling::GetMenuShortcut(String^ commandId, System::Windows::Forms::ToolStripItemCollection^ items)
+{
+	ToolStripMenuItem^ menu = GetMenuItem(commandId, items);
+
+	if (menu != nullptr)
+		return menu->ShortcutKeys;
+
+	return System::Windows::Forms::Keys::None;
+}
+
+bool CommandHandling::SetMenuShortcut(String^ commandId, Keys keyPress, ToolStripItemCollection^ items)
+{
+	ToolStripMenuItem^ menu = GetMenuItem(commandId, items);
+
+	if (menu != nullptr)
+	{ 
+		menu->ShortcutKeys = keyPress;
+		return true;
+	}
+
+	return false;
+}
+
+ToolStripMenuItem^ CommandHandling::GetMenuItem(String^ commandId, ToolStripItemCollection^ items)
+{
+	ToolStripItem^ cmd = GetCommandItem(commandId, items);
+
+	if (cmd != nullptr)
+		return dynamic_cast<ToolStripMenuItem^>(cmd);
+
+	// else
+	return nullptr;
+}
+
+ToolStripItem^ CommandHandling::GetCommandItem(String^ commandId, ToolStripItemCollection^ items)
+{
+	int nItem = items->Count;
+
+	while (nItem--)
+	{
+		ToolStripItem^ cmd = items[nItem];
+
+		if (cmd->Name == commandId)
+			return cmd;
+
+		// else sub menu items
+		ToolStripMenuItem^ menu = dynamic_cast<ToolStripMenuItem^>(cmd);
+
+		if ((menu != nullptr) && menu->HasDropDownItems)
+		{
+			cmd = GetCommandItem(commandId, menu->DropDownItems); // RECURSIVE CALL
+
+			if (cmd != nullptr)
+				return cmd;
+		}
+	}
+
+	return nullptr;
+}
+
+ToolStripMenuItem^ CommandHandling::GetMenuItem(Keys keyPress, ToolStripItemCollection^ items)
+{
 	int nItem = items->Count;
 
 	while (nItem--)
 	{
 		ToolStripMenuItem^ menu = dynamic_cast<ToolStripMenuItem^>(items[nItem]);
 
-		if ((menu != nullptr) && menu->Enabled)
+		if (menu != nullptr)
 		{
 			if (menu->ShortcutKeys == keyPress)
+				return menu;
+
+			// sub menu items
+			if (menu->HasDropDownItems)
 			{
-				// do command
-				menu->PerformClick();
-				return true;
-			}
-			else  if (menu->HasDropDownItems)
-			{
-				if (ProcessMenuShortcut(keyPress, menu->DropDownItems))
-					return true;
+				menu = GetMenuItem(keyPress, menu->DropDownItems); // RECURSIVE CALL
+
+				if (menu != nullptr)
+					return menu;
 			}
 		}
 	}
 
-	return false;
+	return nullptr;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
--- PluginHelpers/CommandHandling.h
+++ PluginHelpers/CommandHandling.h
@@ -21,6 +21,12 @@ namespace Abstractspoon
 			public:
 				static bool HideCommand(String^ commandId, System::Windows::Forms::ToolStripItemCollection^ items);
 				static bool ProcessMenuShortcut(System::Windows::Forms::Keys keyPress, System::Windows::Forms::ToolStripItemCollection^ items);
+				static bool SetMenuShortcut(String^ commandId, System::Windows::Forms::Keys keyPress, System::Windows::Forms::ToolStripItemCollection^ items);
+
+				static System::Windows::Forms::Keys GetMenuShortcut(String^ commandId, System::Windows::Forms::ToolStripItemCollection^ items);
+				static System::Windows::Forms::ToolStripItem^ GetCommandItem(String^ commandId, System::Windows::Forms::ToolStripItemCollection^ items);
+				static System::Windows::Forms::ToolStripMenuItem^ GetMenuItem(String^ commandId, System::Windows::Forms::ToolStripItemCollection^ items);
+				static System::Windows::Forms::ToolStripMenuItem^ GetMenuItem(System::Windows::Forms::Keys keyPress, System::Windows::Forms::ToolStripItemCollection^ items);
 			};
 		}
 	}
