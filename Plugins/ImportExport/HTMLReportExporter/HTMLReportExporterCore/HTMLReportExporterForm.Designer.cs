8ad612bf196f83d0cfaaafccade07e307b3d280c
abstractspoon <daniel.godson@gmail.com>
2019-05-13 21:23:22 +1000

    Experiment with form design

--- Plugins/ImportExport/HTMLReportExporter/HTMLReportExporterCore/HTMLReportExporterForm.Designer.cs
+++ Plugins/ImportExport/HTMLReportExporter/HTMLReportExporterCore/HTMLReportExporterForm.Designer.cs
@@ -29,9 +29,16 @@
 		private void InitializeComponent()
 		{
 			this.Content = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
 			this.BtnOK = new System.Windows.Forms.Button();
 			this.BtnCancel = new System.Windows.Forms.Button();
-			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.tdlHtmlReportControl1 = new HTMLReportExporter.TDLHtmlReportControl();
+			this.tdlHtmlReportControl2 = new HTMLReportExporter.TDLHtmlReportControl();
+			this.tdlHtmlReportControl3 = new HTMLReportExporter.TDLHtmlReportControl();
+			this.tdlHtmlReportControl4 = new HTMLReportExporter.TDLHtmlReportControl();
 			this.Content.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -40,13 +47,56 @@
 			this.Content.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.Content.Controls.Add(this.comboBox1);
+			this.Content.Controls.Add(this.tdlHtmlReportControl4);
+			this.Content.Controls.Add(this.tdlHtmlReportControl3);
+			this.Content.Controls.Add(this.tdlHtmlReportControl2);
+			this.Content.Controls.Add(this.tdlHtmlReportControl1);
+			this.Content.Controls.Add(this.label4);
+			this.Content.Controls.Add(this.label3);
+			this.Content.Controls.Add(this.label2);
+			this.Content.Controls.Add(this.label1);
 			this.Content.Controls.Add(this.BtnOK);
 			this.Content.Controls.Add(this.BtnCancel);
 			this.Content.Location = new System.Drawing.Point(0, 0);
 			this.Content.Name = "Content";
 			this.Content.Size = new System.Drawing.Size(787, 436);
 			this.Content.TabIndex = 0;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(13, 319);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(65, 13);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Page Footer";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(13, 181);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(66, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Task Layout";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 97);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(62, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Report Title";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(70, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Page Header";
 			// 
 			// BtnOK
 			// 
@@ -71,13 +121,53 @@
 			this.BtnCancel.Text = "Cancel";
 			this.BtnCancel.UseVisualStyleBackColor = true;
 			// 
-			// comboBox1
+			// tdlHtmlReportControl1
 			// 
-			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Location = new System.Drawing.Point(0, 0);
-			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(121, 21);
-			this.comboBox1.TabIndex = 4;
+			this.tdlHtmlReportControl1.EditEnabled = true;
+			this.tdlHtmlReportControl1.InnerText = null;
+			this.tdlHtmlReportControl1.LastBrowsedFileFolder = null;
+			this.tdlHtmlReportControl1.LastBrowsedImageFolder = null;
+			this.tdlHtmlReportControl1.Location = new System.Drawing.Point(16, 29);
+			this.tdlHtmlReportControl1.Name = "tdlHtmlReportControl1";
+			this.tdlHtmlReportControl1.Size = new System.Drawing.Size(675, 64);
+			this.tdlHtmlReportControl1.TabIndex = 6;
+			this.tdlHtmlReportControl1.ToolbarDock = System.Windows.Forms.DockStyle.Top;
+			// 
+			// tdlHtmlReportControl2
+			// 
+			this.tdlHtmlReportControl2.EditEnabled = true;
+			this.tdlHtmlReportControl2.InnerText = null;
+			this.tdlHtmlReportControl2.LastBrowsedFileFolder = null;
+			this.tdlHtmlReportControl2.LastBrowsedImageFolder = null;
+			this.tdlHtmlReportControl2.Location = new System.Drawing.Point(16, 114);
+			this.tdlHtmlReportControl2.Name = "tdlHtmlReportControl2";
+			this.tdlHtmlReportControl2.Size = new System.Drawing.Size(675, 64);
+			this.tdlHtmlReportControl2.TabIndex = 6;
+			this.tdlHtmlReportControl2.ToolbarDock = System.Windows.Forms.DockStyle.Top;
+			// 
+			// tdlHtmlReportControl3
+			// 
+			this.tdlHtmlReportControl3.EditEnabled = true;
+			this.tdlHtmlReportControl3.InnerText = null;
+			this.tdlHtmlReportControl3.LastBrowsedFileFolder = null;
+			this.tdlHtmlReportControl3.LastBrowsedImageFolder = null;
+			this.tdlHtmlReportControl3.Location = new System.Drawing.Point(16, 197);
+			this.tdlHtmlReportControl3.Name = "tdlHtmlReportControl3";
+			this.tdlHtmlReportControl3.Size = new System.Drawing.Size(675, 119);
+			this.tdlHtmlReportControl3.TabIndex = 6;
+			this.tdlHtmlReportControl3.ToolbarDock = System.Windows.Forms.DockStyle.Top;
+			// 
+			// tdlHtmlReportControl4
+			// 
+			this.tdlHtmlReportControl4.EditEnabled = true;
+			this.tdlHtmlReportControl4.InnerText = null;
+			this.tdlHtmlReportControl4.LastBrowsedFileFolder = null;
+			this.tdlHtmlReportControl4.LastBrowsedImageFolder = null;
+			this.tdlHtmlReportControl4.Location = new System.Drawing.Point(16, 335);
+			this.tdlHtmlReportControl4.Name = "tdlHtmlReportControl4";
+			this.tdlHtmlReportControl4.Size = new System.Drawing.Size(675, 64);
+			this.tdlHtmlReportControl4.TabIndex = 6;
+			this.tdlHtmlReportControl4.ToolbarDock = System.Windows.Forms.DockStyle.Top;
 			// 
 			// HTMLReportExporterForm
 			// 
@@ -89,6 +179,7 @@
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "HTMLReportExporterForm";
 			this.Content.ResumeLayout(false);
+			this.Content.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -98,6 +189,13 @@
 		private System.Windows.Forms.Panel Content;
 		private System.Windows.Forms.Button BtnOK;
 		private System.Windows.Forms.Button BtnCancel;
-		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private TDLHtmlReportControl tdlHtmlReportControl1;
+		private TDLHtmlReportControl tdlHtmlReportControl4;
+		private TDLHtmlReportControl tdlHtmlReportControl3;
+		private TDLHtmlReportControl tdlHtmlReportControl2;
 	}
 }
