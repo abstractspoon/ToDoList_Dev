5fe6061d17b5ae1add3e0f23ca5bf3a3edf0b13a
dan.g <abstractspoon@users.noreply.github.com>
2019-06-06 15:55:42 +1000

    Add placeholder for colour selector button

---
+++ Plugins/3rdParty/MSHtmlEditorControl/Classes/ColorButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MSDN.Html.Editor.Classes
+{
+	public partial class ColorButton : Button
+	{
+		public ColorButton()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnPaint(PaintEventArgs pe)
+		{
+			base.OnPaint(pe);
+		}
+	}
+}
