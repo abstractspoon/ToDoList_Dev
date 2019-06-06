e2bb513e4bb7c0b8cc4672ba887e30abb02045f6
dan.g <abstractspoon@users.noreply.github.com>
2019-06-06 15:55:24 +1000

    Add placeholder dialogs for row/cell properties

---
+++ Plugins/3rdParty/MSHtmlEditorControl/Forms/TableRowPropertyForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MSDN.Html.Editor.Forms
+{
+	public partial class TableRowPropertyForm : Form
+	{
+		public TableRowPropertyForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
