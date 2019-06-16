39ec3da9b9fc481bb61ac6f27449f7ac1d5dc95b
abstractspoon <daniel.godson@gmail.com>
2019-06-16 15:23:06 +1000

    Add agilitypack to zip

--- BuildReleaseZip.bat
+++ BuildReleaseZip.bat
@@ -75,6 +75,7 @@ REM .Net Plugins are obfuscated first
 %PATH7ZIP% a %OUTZIP% %OUTDIR%\MSDN.HtmlEditorControl.dll"
 %PATH7ZIP% a %OUTZIP% %OUTDIR%\HTMLReportExporterBridge.dll"
 %PATH7ZIP% a %OUTZIP% %OUTDIR%\HTMLReportExporterCore.dll"
+%PATH7ZIP% a %OUTZIP% %OUTDIR%\HtmlAgilityPack.dll"
 
 REM Copy latest Resources
 del %OUTDIR%\Resources\ /Q /S
