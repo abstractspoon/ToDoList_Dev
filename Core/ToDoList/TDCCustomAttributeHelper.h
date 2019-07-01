55e2af66207c6a400fe44ab512b7ad4c6331816a
abstractspoon <daniel.godson@gmail.com>
2019-07-01 19:49:02 +1000

    Add generic hints to custom attributes

--- Core/ToDoList/TDCCustomAttributeHelper.h
+++ Core/ToDoList/TDCCustomAttributeHelper.h
@@ -16,31 +16,36 @@
 class CTDCCustomAttribDefinitionArray;
 class CTDCCustomControlArray;
 class CTDCImageList;
+class CWndPromptManager;
 
 //////////////////////////////////////////////////////////////////////
 
-class CTDCCustomAttributeHelper  
+class CTDCCustomAttributeHelper
 {
 public:
-	static BOOL RebuildEditControls(const CTDCCustomAttribDefinitionArray& aAttribDefs, 
-									CTDCCustomControlArray& aControls, 
-									const CTDCImageList& ilImages, 
-									CWnd* pParent, UINT nCtrlIDPos);
-	static BOOL RebuildFilterControls(const CTDCCustomAttribDefinitionArray& aAttribDefs, 
-									CTDCCustomControlArray& aControls, 
-									const CTDCImageList& ilImages, 
-									CWnd* pParent, UINT nCtrlIDPos,
-									BOOL bMultiSelection);
+	static BOOL RebuildEditControls(const CTDCCustomAttribDefinitionArray& aAttribDefs,
+									const CTDCImageList& ilImages,
+									CWnd* pParent, 
+									UINT nCtrlIDPos,
+									CTDCCustomControlArray& aControls);
 
-	static BOOL NeedRebuildEditControls(const CTDCCustomAttribDefinitionArray& aOldAttribDefs, 
-										const CTDCCustomAttribDefinitionArray& aNewAttribDefs, 
+	static BOOL RebuildFilterControls(const CTDCCustomAttribDefinitionArray& aAttribDefs,
+									  const CTDCImageList& ilImages,
+									  CWnd* pParent, 
+									  UINT nCtrlIDPos,
+									  BOOL bMultiSelection,
+									  CTDCCustomControlArray& aControls);
+
+	static BOOL NeedRebuildEditControls(const CTDCCustomAttribDefinitionArray& aOldAttribDefs,
+										const CTDCCustomAttribDefinitionArray& aNewAttribDefs,
 										const CTDCCustomControlArray& aOldControls);
 
-	static BOOL NeedRebuildFilterControls(const CTDCCustomAttribDefinitionArray& aOldAttribDefs, 
-											const CTDCCustomAttribDefinitionArray& aNewAttribDefs, 
-											const CTDCCustomControlArray& aOldControls);
+	static BOOL NeedRebuildFilterControls(const CTDCCustomAttribDefinitionArray& aOldAttribDefs,
+										  const CTDCCustomAttribDefinitionArray& aNewAttribDefs,
+										  const CTDCCustomControlArray& aOldControls);
 
 	static void CleanupControls(CTDCCustomControlArray& aControls, CWnd* pParent);
+	static void AddWindowPrompts(const CTDCCustomControlArray& aControls, CWnd* pParent, CWndPromptManager& mgrPrompts);
 
 	static int EnableMultiSelectionFilter(const CTDCCustomControlArray& aControls, CWnd* pParent, BOOL bEnable = TRUE);
 	static void ClearFilterCheckboxHistory(const CTDCCustomControlArray& aControls, CWnd* pParent);
@@ -49,7 +54,7 @@ public:
 								 const CTDCCustomControlArray& aControls);
 
 	// -------------------------------------------------------------------------
-	
+
 	static CString GetAttributeTypeID(UINT nCtrlID, const CTDCCustomControlArray& aControls);
 	static CString GetAttributeTypeID(TDC_COLUMN nColID, const CTDCCustomAttribDefinitionArray& aAttribDefs);
 	static CString GetAttributeTypeID(TDC_ATTRIBUTE nAttribID, const CTDCCustomAttribDefinitionArray& aAttribDefs);
@@ -64,73 +69,70 @@ public:
 	static CString GetFilterControlTooltip(UINT nCtrlID, CWnd* pParent);
 	static CString GetEditControlTooltip(UINT nCtrlID, CWnd* pParent);
 
-	static BOOL GetAttributeDef(TDC_COLUMN nColID, 
+	static BOOL GetAttributeDef(TDC_COLUMN nColID,
 								const CTDCCustomAttribDefinitionArray& aAttribDefs,
 								TDCCUSTOMATTRIBUTEDEFINITION& attribDef);
 
-	static BOOL GetAttributeDef(TDC_ATTRIBUTE nAttribID, 
+	static BOOL GetAttributeDef(TDC_ATTRIBUTE nAttribID,
 								const CTDCCustomAttribDefinitionArray& aAttribDefs,
 								TDCCUSTOMATTRIBUTEDEFINITION& attribDef);
 
-	static BOOL GetAttributeDef(const CUSTOMATTRIBCTRLITEM& ctrl, 
+	static BOOL GetAttributeDef(const CUSTOMATTRIBCTRLITEM& ctrl,
 								const CTDCCustomAttribDefinitionArray& aAttribDefs,
 								TDCCUSTOMATTRIBUTEDEFINITION& attribDef);
 
-	static BOOL GetAttributeDef(const CString& sUniqueID, 
+	static BOOL GetAttributeDef(const CString& sUniqueID,
 								const CTDCCustomAttribDefinitionArray& aAttribDefs,
 								TDCCUSTOMATTRIBUTEDEFINITION& attribDef);
 
-	static DWORD GetAttributeDataType(const CString& sUniqueID, 
-										const CTDCCustomAttribDefinitionArray& aAttribDefs);
+	static DWORD GetAttributeDataType(const CString& sUniqueID,
+									  const CTDCCustomAttribDefinitionArray& aAttribDefs);
 
-	static CString FormatData(const TDCCADATA& data, const CString& sUniqueID, 
-								const CTDCCustomAttribDefinitionArray& aAttribDefs);
+	static CString FormatData(const TDCCADATA& data, const CString& sUniqueID,
+							  const CTDCCustomAttribDefinitionArray& aAttribDefs);
 	static CString FormatData(const TDCCADATA& data, const TDCCUSTOMATTRIBUTEDEFINITION& attribDef);
 
-	static TDC_ATTRIBUTE GetAttributeID(TDC_COLUMN nColID, 
+	static TDC_ATTRIBUTE GetAttributeID(TDC_COLUMN nColID,
 										const CTDCCustomAttribDefinitionArray& aAttribDefs);
 
-	static TDC_ATTRIBUTE GetAttributeID(const CString& sUniqueID, 
+	static TDC_ATTRIBUTE GetAttributeID(const CString& sUniqueID,
 										const CTDCCustomAttribDefinitionArray& aAttribDefs);
 
-	static BOOL IsColumnSortable(TDC_COLUMN nColID, 
-								const CTDCCustomAttribDefinitionArray& aAttribDefs);
-	
-	// not currently used
-	static CWnd* GetControlFromAttributeDef(const CWnd* pParent, const TDCCUSTOMATTRIBUTEDEFINITION& attribDef,
-											const CTDCCustomControlArray& aControls);
+	static BOOL IsColumnSortable(TDC_COLUMN nColID,
+								 const CTDCCustomAttribDefinitionArray& aAttribDefs);
 
-	static void SaveAutoListDataToDefs(const CWnd* pParent, const CTDCCustomControlArray& aControls, 
-										const CTDCCustomAttribDefinitionArray& aAttribDefs);
+	static void SaveAutoListDataToDefs(const CWnd* pParent, const CTDCCustomControlArray& aControls,
+									   const CTDCCustomAttribDefinitionArray& aAttribDefs);
 
 	static BOOL GetControl(UINT nCtrlID, const CTDCCustomControlArray& aControls, CUSTOMATTRIBCTRLITEM& ctrl);
 	static BOOL GetControl(const CString& sUniqueID, const CTDCCustomControlArray& aControls, CUSTOMATTRIBCTRLITEM& ctrl);
 
 	static void UpdateControl(const CWnd* pParent, const CUSTOMATTRIBCTRLITEM& ctrl,
-								const CTDCCustomAttribDefinitionArray& aAttribDefs,
-								const TDCCADATA& data);
+							  const CTDCCustomAttribDefinitionArray& aAttribDefs,
+							  const TDCCADATA& data);
 
 	static void ClearControl(const CWnd* pParent, const CUSTOMATTRIBCTRLITEM& ctrl,
-								const CTDCCustomAttribDefinitionArray& aAttribDefs);
+							 const CTDCCustomAttribDefinitionArray& aAttribDefs);
 
-	static void UpdateControls(const CWnd* pParent, const CTDCCustomControlArray& aControls,
-								const CTDCCustomAttribDefinitionArray& aAttribDefs,
-								const CTDCCustomAttributeDataMap& mapData);
+	static void UpdateControls(const CWnd* pParent,
+							   const CTDCCustomControlArray& aControls,
+							   const CTDCCustomAttribDefinitionArray& aAttribDefs,
+							   const CTDCCustomAttributeDataMap& mapData);
 
 	static void ClearControls(const CWnd* pParent, const CTDCCustomControlArray& aControls,
-								const CTDCCustomAttribDefinitionArray& aAttribDefs);
+							  const CTDCCustomAttribDefinitionArray& aAttribDefs);
 
 	static BOOL GetControlAttributeTypes(const CUSTOMATTRIBCTRLITEM& ctrl,
-										const CTDCCustomAttribDefinitionArray& aAttribDefs,
-										DWORD& dwDataType, DWORD& dwListType);
+										 const CTDCCustomAttribDefinitionArray& aAttribDefs,
+										 DWORD& dwDataType, DWORD& dwListType);
 
 	static BOOL GetControlData(const CWnd* pParent, const CUSTOMATTRIBCTRLITEM& ctrl,
-								const CTDCCustomAttribDefinitionArray& aAttribDefs,
-								TDCCADATA& data);
+							   const CTDCCustomAttribDefinitionArray& aAttribDefs,
+							   TDCCADATA& data);
 
 	static BOOL GetControlData(const CWnd* pParent, const CTDCCustomControlArray& aControls,
-								const CTDCCustomAttribDefinitionArray& aAttribDefs,
-								CTDCCustomAttributeDataMap& mapData);
+							   const CTDCCustomAttribDefinitionArray& aAttribDefs,
+							   CTDCCustomAttributeDataMap& mapData);
 
 	static FIND_ATTRIBTYPE GetAttributeFindType(const CString& sUniqueID, BOOL bRelativeDate,
 												const CTDCCustomAttribDefinitionArray& aAttribDefs);
@@ -138,36 +140,40 @@ public:
 	static FIND_ATTRIBTYPE GetAttributeFindType(TDC_ATTRIBUTE nAttribID, BOOL bRelativeDate,
 												const CTDCCustomAttribDefinitionArray& aAttribDefs);
 
-	static BOOL AppendFilterRules(const CTDCCustomAttributeDataMap& mapData, 
-								const CTDCCustomAttribDefinitionArray& aAttribDefs, CSearchParamArray& aRules);
+	static BOOL AppendFilterRules(const CTDCCustomAttributeDataMap& mapData,
+								  const CTDCCustomAttribDefinitionArray& aAttribDefs, CSearchParamArray& aRules);
 
 	static int GetVisibleColumnIDs(const CTDCCustomAttribDefinitionArray& aAttribDefs,
-									CTDCColumnIDMap& mapCols, BOOL bAppend = TRUE);
+								   CTDCColumnIDMap& mapCols, BOOL bAppend = TRUE);
 
-	static void DDX(CDataExchange* pDX, const CTDCCustomControlArray& aControls, 
+	static void DDX(CDataExchange* pDX, const CTDCCustomControlArray& aControls,
 					const CTDCCustomAttribDefinitionArray& aAttribDefs, CTDCCustomAttributeDataMap& value);
 
+	// not currently used
+	static CWnd* GetControlFromAttributeDef(const CWnd* pParent, const TDCCUSTOMATTRIBUTEDEFINITION& attribDef,
+											const CTDCCustomControlArray& aControls);
+
 protected:
-	static CWnd* CreateAttribute(const TDCCUSTOMATTRIBUTEDEFINITION& attribDef, 
-								const CTDCImageList& ilImages, CWnd* pParent, 
-								UINT nCtrlID, BOOL bBuddy, BOOL bFilter, BOOL bMultiSelectionFilter);
-	
-	static CWnd* CreateAttributeLabel(const TDCCUSTOMATTRIBUTEDEFINITION& attribDef, 
-										CWnd* pParent, UINT nCtrlID, BOOL bBuddy);
-	
+	static CWnd* CreateAttribute(const TDCCUSTOMATTRIBUTEDEFINITION& attribDef,
+								 const CTDCImageList& ilImages, CWnd* pParent,
+								 UINT nCtrlID, BOOL bBuddy, BOOL bFilter, BOOL bMultiSelectionFilter);
+
+	static CWnd* CreateAttributeLabel(const TDCCUSTOMATTRIBUTEDEFINITION& attribDef,
+									  CWnd* pParent, UINT nCtrlID, BOOL bBuddy);
+
 	static BOOL AttributeWantsBuddy(const TDCCUSTOMATTRIBUTEDEFINITION& attribDef);
 	static CString GetControlLabel(const TDCCUSTOMATTRIBUTEDEFINITION& attribDef, BOOL bBuddy);
 
-	static BOOL RebuildControls(const CTDCCustomAttribDefinitionArray& aAttribDefs, 
-								CTDCCustomControlArray& aControls, 
-								const CTDCImageList& ilImages, CWnd* pParent, 
-								UINT nCtrlIDPos, UINT nCtrlIDStart, 
-								BOOL bFilter, BOOL bMultiSelectionFilter);
-	static BOOL NeedRebuildControls(const CTDCCustomAttribDefinitionArray& aOldAttribDefs, 
-									const CTDCCustomAttribDefinitionArray& aNewAttribDefs, 
+	static BOOL RebuildControls(const CTDCCustomAttribDefinitionArray& aAttribDefs,
+								const CTDCImageList& ilImages, CWnd* pParent,
+								UINT nCtrlIDPos, UINT nCtrlIDStart,
+								BOOL bFilter, BOOL bMultiSelectionFilter,
+								CTDCCustomControlArray& aControls);
+	static BOOL NeedRebuildControls(const CTDCCustomAttribDefinitionArray& aOldAttribDefs,
+									const CTDCCustomAttribDefinitionArray& aNewAttribDefs,
 									const CTDCCustomControlArray& aOldControls, UINT nCtrlIDStart, BOOL bFilter);
-	static int GetCustomAttributeCtrls(const CTDCCustomAttribDefinitionArray& aAttribDefs, 
-									CTDCCustomControlArray& aControls, UINT nCtrlIDStart, BOOL bFilter);
+	static int GetCustomAttributeCtrls(const CTDCCustomAttribDefinitionArray& aAttribDefs,
+									   CTDCCustomControlArray& aControls, UINT nCtrlIDStart, BOOL bFilter);
 	static BOOL WantControl(const TDCCUSTOMATTRIBUTEDEFINITION& attribDef, BOOL bFilter);
 	static CString GetControlTooltip(UINT nCtrlID, CWnd* pParent);
 	static CString FormatNumber(double dValue, const TDCCUSTOMATTRIBUTEDEFINITION& attribDef);
