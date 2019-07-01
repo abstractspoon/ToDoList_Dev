1f2c7afe373208bf0ddbf991079750518da9f692
dan.g <abstractspoon@users.noreply.github.com>
2019-07-01 13:29:43 +1000

    Add 'WorkingDay' helper class

--- Plugins/PluginHelpers/DateUtil.cpp
+++ Plugins/PluginHelpers/DateUtil.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "DateUtil.h"
+#include "Preferences.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -10,6 +11,156 @@ using namespace Abstractspoon::Tdl::PluginHelpers;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
+WorkingDay::WorkingDay() 
+	: 
+	m_StartOfDayInHours(9), 
+	m_StartOfLunchInHours(13),
+	m_EndOfLunchInHours(14),
+	m_EndOfDayInHours(18)
+{
+
+}
+
+WorkingDay::WorkingDay(Preferences^ prefs)
+	:
+	m_StartOfDayInHours(9),
+	m_StartOfLunchInHours(12.5),
+	m_EndOfLunchInHours(13.5),
+	m_EndOfDayInHours(17.5)
+{
+	Load(prefs);
+}
+
+double WorkingDay::StartOfDayInHours()
+{
+	return m_StartOfDayInHours;
+}
+
+double WorkingDay::EndOfDayInHours()
+{
+	return m_EndOfDayInHours;
+}
+
+double WorkingDay::StartOfLunchInHours()
+{
+	return m_StartOfLunchInHours;
+}
+
+double WorkingDay::EndOfLunchInHours()
+{
+	return m_EndOfLunchInHours;
+}
+
+DateTime^ WorkingDay::StartOfDay(DateTime^ date)
+{
+	return date->Date.AddMilliseconds(HoursToMilleseconds(m_StartOfDayInHours));
+}
+
+DateTime^ WorkingDay::EndOfDay(DateTime^ date)
+{
+	return date->Date.AddMilliseconds(HoursToMilleseconds(m_EndOfDayInHours));
+}
+
+DateTime^ WorkingDay::StartOfLunch(DateTime^ date)
+{
+	return date->Date.AddMilliseconds(HoursToMilleseconds(m_StartOfLunchInHours));
+}
+
+DateTime^ WorkingDay::EndOfLunch(DateTime^ date)
+{
+	return date->Date.AddMilliseconds(HoursToMilleseconds(m_EndOfLunchInHours));
+}
+
+Int32 WorkingDay::HoursToMilleseconds(double hours)
+{
+	return (Int32)(hours * 60 * 60 * 1000);
+}
+
+double WorkingDay::CalculateDurationInHours(System::DateTime^ from, DateTime^ to)
+{
+	int nDaysDuration = (to->Date - from->Date).Days;
+
+	if (nDaysDuration < 0)
+		return 0;
+
+	double fromTimeOfDay = GetTimeOfDayInHours(from);
+	double toTimeOfDay = GetTimeOfDayInHours(to);
+	double dHoursDuration = 0;
+	
+	if (nDaysDuration == 0)
+	{
+		// from and to are same day
+		dHoursDuration = CalculateDurationInHours(fromTimeOfDay, toTimeOfDay);
+	}
+	else
+	{
+		// clear days
+		dHoursDuration = ((nDaysDuration - 1) * DayLengthInHours());
+
+		// part days
+		dHoursDuration += CalculateDurationInHours(fromTimeOfDay, m_EndOfDayInHours);
+		dHoursDuration += CalculateDurationInHours(m_StartOfDayInHours, toTimeOfDay);
+	}
+
+	return dHoursDuration;
+}
+
+double WorkingDay::CalculateDurationInHours(double fromHour, double toHour)
+{
+	fromHour = Math::Max(fromHour, m_StartOfDayInHours);
+	toHour = Math::Min(toHour, m_EndOfDayInHours);
+
+	if (fromHour >= toHour)
+		return 0;
+
+	if (fromHour >= m_StartOfLunchInHours)
+		fromHour = Math::Max(fromHour, m_EndOfLunchInHours);
+
+	if (toHour <= m_EndOfLunchInHours)
+		toHour = Math::Min(toHour, m_StartOfLunchInHours);
+
+	double dDuration = (toHour - fromHour);
+
+	if (fromHour < m_StartOfLunchInHours && toHour > m_EndOfLunchInHours)
+		dDuration -= LunchBreakInHours();
+
+	return Math::Max(dDuration, 0.0);
+}
+
+double WorkingDay::GetTimeOfDayInHours(DateTime^ date)
+{
+	return (double)(date->Hour + (date->Minute / 60.0) + (date->Second / 3600.0) + (date->Millisecond / 3600000));
+}
+
+double WorkingDay::DayLengthInHours()
+{
+	return (m_EndOfDayInHours - m_StartOfDayInHours - LunchBreakInHours());
+}
+
+double WorkingDay::LunchBreakInHours()
+{
+	return (m_EndOfLunchInHours - m_StartOfLunchInHours);
+}
+
+void WorkingDay::Load(Preferences^ prefs)
+{
+	m_StartOfDayInHours = prefs->GetProfileDouble(L"Preferences", L"StartOfWorkdayInHours", 9.0);
+	m_StartOfLunchInHours = prefs->GetProfileDouble(L"Preferences", L"StartOfLunchInHours", 13.0);
+	m_EndOfLunchInHours = prefs->GetProfileDouble(L"Preferences", L"EndOfLunchInHours", 14.0);
+
+	m_EndOfDayInHours = (m_StartOfDayInHours + 
+						(m_EndOfLunchInHours - m_StartOfLunchInHours) + 
+						prefs->GetProfileDouble(L"Preferences", L"HoursInDay", 8.0));
+
+	if ((m_StartOfLunchInHours < m_StartOfDayInHours) ||
+		(m_EndOfLunchInHours > m_EndOfDayInHours) ||
+		(m_StartOfLunchInHours > m_EndOfLunchInHours))
+	{
+		m_StartOfLunchInHours = m_EndOfLunchInHours;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 /// <summary>
 /// Get the Week number of the year
--- Plugins/PluginHelpers/DateUtil.h
+++ Plugins/PluginHelpers/DateUtil.h
@@ -12,6 +12,43 @@ namespace Abstractspoon
 	{
 		namespace PluginHelpers
 		{
+			ref class Preferences;
+
+			public ref class WorkingDay
+			{
+				WorkingDay();
+				WorkingDay(Preferences^ prefs);
+
+				double StartOfDayInHours();
+				double EndOfDayInHours();
+				double StartOfLunchInHours();
+				double EndOfLunchInHours();
+				
+				DateTime^ StartOfDay(DateTime^ date);
+				DateTime^ EndOfDay(DateTime^ date);
+				DateTime^ StartOfLunch(DateTime^ date);
+				DateTime^ EndOfLunch(DateTime^ date);
+
+				double CalculateDurationInHours(DateTime^ from, DateTime^ to);
+				double DayLengthInHours();
+				double LunchBreakInHours();
+
+				void Load(Preferences^ prefs);
+
+			protected:
+				double m_StartOfDayInHours;
+				double m_EndOfDayInHours;
+				double m_StartOfLunchInHours;
+				double m_EndOfLunchInHours;
+
+			protected:
+				double CalculateDurationInHours(double fromHour, double toHour);
+
+				static Int32 HoursToMilleseconds(double hours);
+				static double GetTimeOfDayInHours(DateTime^ date);
+
+			};
+			
 			public ref class DateUtil
 			{
 			public:
