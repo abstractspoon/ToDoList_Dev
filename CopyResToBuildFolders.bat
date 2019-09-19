7abf4e05e72975e39adece3d77303d97e5fa6ebb
dan.g <abstractspoon@users.noreply.github.com>
2019-09-19 11:48:46 +1000

    Copy test tasklists also

--- CopyResToBuildFolders.bat
+++ CopyResToBuildFolders.bat
@@ -7,13 +7,22 @@ ECHO REPO=%REPO%
 set RESREPO=%REPO%\..\ToDoList_Resources
 ECHO RESREPO=%RESREPO%
 
+set TESTREPO=%REPO%\..\ToDoList_Test
+ECHO TESTREPO=%TESTREPO%
+
 REM - Copy to Core
 xcopy %RESREPO%\*.* %REPO%\Core\ToDoList\Unicode_Debug\Resources\   /E /EXCLUDE:%REPO%\CopyResToBuildFolders_Exclude.txt /Y 
 xcopy %RESREPO%\*.* %REPO%\Core\ToDoList\Unicode_Release\Resources\   /E /EXCLUDE:%REPO%\CopyResToBuildFolders_Exclude.txt /Y 
 
+xcopy %TESTREPO%\Tasklists\*.* %REPO%\Core\ToDoList\Unicode_Debug\Resources\Examples\  /Y   
+xcopy %TESTREPO%\Tasklists\*.* %REPO%\Core\ToDoList\Unicode_Release\Resources\Examples\  /Y   
+
 REM - Copy Resources to Plugins
 xcopy %RESREPO%\*.* %REPO%\Plugins\Debug\Resources\   /E /EXCLUDE:%REPO%\CopyResToBuildFolders_Exclude.txt /Y 
 xcopy %RESREPO%\*.* %REPO%\Plugins\Release\Resources\ /E /EXCLUDE:%REPO%\CopyResToBuildFolders_Exclude.txt /Y 
+
+xcopy %TESTREPO%\Tasklists\*.* %REPO%\Plugins\Debug\Resources\Examples\  /Y   
+xcopy %TESTREPO%\Tasklists\*.* %REPO%\Plugins\Release\Resources\Examples\  /Y   
 
 popd
 pause
