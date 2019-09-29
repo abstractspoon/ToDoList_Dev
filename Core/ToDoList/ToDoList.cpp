e45c595636907bf14989be7eb287c8cfb288ae8c
abstractspoon <daniel.godson@gmail.com>
2019-09-29 19:58:06 +1000

    Fully qualify dll path

--- Core/ToDoList/ToDoList.cpp
+++ Core/ToDoList/ToDoList.cpp
@@ -128,7 +128,10 @@ CToDoListApp theApp;
 BOOL CToDoListApp::InitInstance()
 {
 	// .NET plugins require VS2010 redistributable to be installed
-	if (::LoadLibrary(MSVCR100_DLL) == NULL)
+	CString sVs2010Runtime;
+	VERIFY(FileMisc::GetSpecialFilePath(CSIDL_SYSTEM, MSVCR100_DLL, sVs2010Runtime));
+
+	if (::LoadLibrary(sVs2010Runtime) == NULL)
 	{
 		if (DoMessageBox(MSVCR100_MSG, MB_OKCANCEL | MB_ICONEXCLAMATION) == IDOK)
 			FileMisc::Run(::GetDesktopWindow(), MSVCR100_URL);
@@ -136,6 +139,19 @@ BOOL CToDoListApp::InitInstance()
 		// Always quit
 		return FALSE;
 	}
+
+/*
+	HANDLE hFile = CreateFile(sVs2010Runtime, // file to open
+					   GENERIC_READ,          // open for reading
+					   FILE_SHARE_READ,       // share for reading
+					   NULL,                  // default security
+					   OPEN_EXISTING,         // existing file only
+					   FILE_ATTRIBUTE_NORMAL, // normal file
+					   NULL);                 // no attr. template
+
+	TCHAR szFinal[MAX_PATH + 1];
+	GetFinalPathNameByHandle(hFile, szFinal, MAX_PATH, VOLUME_NAME_DOS);
+*/
 
 	// Set this before anything else
 	CWinHelpButton::SetDefaultIcon(GraphicsMisc::LoadIcon(IDI_HELPBUTTON));
