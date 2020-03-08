b5613aae84f4bcd229c03ee0eb99dd94227dedb0
abstractspoon <daniel.godson@gmail.com>
2020-03-08 14:46:55 +1100

    Avoid having to always rebuild 3rdParty/Shared for plugins

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -13,11 +13,8 @@ ECHO ON
 REM - Make sure caller has updated ToDoList version number
 pause
 
-REM - Always rebuild Core App shared code in VC6 because it may have been previously compiled for VS2010 below
+REM - Build core app in VC6
 cd %REPO%\Core
-"C:\Program Files (x86)\Microsoft Visual Studio\Common\MSDev98\Bin\msdev.exe" .\ToDoList_Core_Shared.dsw /MAKE "ALL - Win32 Unicode Release" /REBUILD
-
-REM - Build rest of core app in VC6
 "C:\Program Files (x86)\Microsoft Visual Studio\Common\MSDev98\Bin\msdev.exe" .\ToDoList_Core.dsw /MAKE "ALL - Win32 Unicode Release" 
 
 REM - Copy ToDoList.pdb/.map to versioned symbols folder
@@ -31,16 +28,16 @@ MKDIR %REPO%\..\ToDoList_Symbols\%TDLVER%
 COPY /Y /B %REPO%\Core\ToDoList\Unicode_Release\ToDoList.pdb %REPO%\..\ToDoList_Symbols\%TDLVER%\
 COPY /Y /B %REPO%\Core\ToDoList\Unicode_Release\ToDoList.map %REPO%\..\ToDoList_Symbols\%TDLVER%\
 
-REM - Rebuild ToDoList_Core\3rdParty and ToDoList_Core\Shared in VS2010 for linking into PluginsHelpers
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" .\ToDoList_Core_Shared.sln /Rebuild "Release"
-
 REM - Build Plugins
 cd %REPO%\Plugins
 
-REM - Rebuild 3rdParty by itself because PluginHelpers is dependent on it
+REM - Build ToDoList_Core\3rdParty and ToDoList_Core\Shared in VS2010 for linking into PluginsHelpers
+"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" .\Core\ToDoList_Core_For_Plugins.sln /Build "Release"
+
+REM - Build 3rdParty by itself because PluginHelpers is dependent on it
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" .\3rdParty.sln /Build "Release"
 
-REM - Rebuild PluginHelpers by itself because everything else is dependent on it
+REM - Build PluginHelpers by itself because everything else is dependent on it
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" .\PluginHelpers.sln /Build "Release"
 
 REM - Build rest of plugins
