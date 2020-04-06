6e82cbde2831e4e94c73e30e0feccddfe8e55315
abstractspoon <daniel.godson@gmail.com>
2020-04-06 14:30:45 +1000

    Merge build fixes from ToDoList_8.0

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -28,13 +28,14 @@ MKDIR %REPO%\..\ToDoList_Symbols\%TDLVER%
 COPY /Y /B %REPO%\Core\ToDoList\Unicode_Release\ToDoList.pdb %REPO%\..\ToDoList_Symbols\%TDLVER%\
 COPY /Y /B %REPO%\Core\ToDoList\Unicode_Release\ToDoList.map %REPO%\..\ToDoList_Symbols\%TDLVER%\
 
+REM - Build plugins using MSBuild for reliability
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
 SET BUILDPARAMS=/t:Build /p:Configuration=Release /m /v:normal /noWarn:MSB3267;MSB3305;LNK4248;CS1762;LNK4221;MSB3026
 SET VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140\
 
+REM - Build ToDoList_Core\3rdParty and ToDoList_Core\Shared in VS2010 for linking into PluginsHelpers
 cd %REPO%\Plugins
 
-REM - Build ToDoList_Core\3rdParty and ToDoList_Core\Shared in VS2010 for linking into PluginsHelpers
 %MSBUILD% .\Core\ToDoList_Core_For_Plugins.sln %BUILDPARAMS%
 
 REM - Build rest of plugins
