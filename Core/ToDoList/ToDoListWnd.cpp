4cec7a1e49a643843ee85b5d883b1fb95b77a20e
abstractspoon <daniel.godson@gmail.com>
2020-04-30 14:23:22 +1000

    Merge source control fix from ToDoList_8.0

--- Core/ToDoList/ToDoListWnd.cpp
+++ Core/ToDoList/ToDoListWnd.cpp
@@ -1857,7 +1857,7 @@ BOOL CToDoListWnd::HandleSaveTasklistError(TDC_FILE& nErr, LPCTSTR szTasklist)
 		FileMisc::LogText(_T("Saving of the file '%s' failed with the error code: %d"), szTasklist, nErr);
 		///////////////////////////////////////////////////////////////////////
 
-		CMessageBox::AfxShow(IDS_SAVETASKLIST_TITLE, sMessage, MB_OK);
+		CMessageBox::AfxShow(IDS_SAVETASKLIST_TITLE, sMessage, MB_OK | MB_ICONEXCLAMATION);
 	}
 	
 	return FALSE; // not handled
@@ -9579,14 +9579,19 @@ void CToDoListWnd::OnToolsCheckout()
 			break;
 		}
 
-		// else handle error
+		// Handle error
 		CString sFilePath(m_mgrToDoCtrls.GetFilePath(nSel));
 
-		if ((nFileRes != TDCF_OTHER) || sCheckedOutTo.IsEmpty())
+		if (nFileRes != TDCF_SSC_CHECKEDOUTBYOTHER)
 		{
+			ASSERT(sCheckedOutTo.IsEmpty());
+
 			HandleSaveTasklistError(nFileRes, sFilePath);
 			break;
 		}
+
+		// Handle 'checked out to other' error
+		ASSERT(!sCheckedOutTo.IsEmpty());
 		
 		SYSTEMTIME stLastMod;
 		FileMisc::GetFileLastModified(sFilePath, stLastMod);
