c3654610a0edf1bf4074a2ad68b61a12bec91788
abstractspoon <daniel.godson@gmail.com>
2020-05-15 16:37:44 +1000

    Refactor styles into separate function

--- Core/ToDoList/TDCToDoCtrlPreferenceHelper.h
+++ Core/ToDoList/TDCToDoCtrlPreferenceHelper.h
@@ -17,9 +17,12 @@ public:
 	static void UpdateToDoCtrl(CFilteredToDoCtrl& tdc, const CPreferencesDlg& prefs, 
 								const CFont& fontMain, CFont& fontTree, CFont& fontComments);
 
+	static void PopulateStyles(const CPreferencesDlg& prefs, CTDCStyleMap& mapStyles);
+
 protected:
 	static void UpdateToDoCtrlPrefs(CFilteredToDoCtrl& tdc, const CPreferencesDlg& prefs, 
 									BOOL bShowProjectName, BOOL bShowTreeListBar);
+
 };
 
 #endif // AFX_TDCTODOCTRLPREFERENCEHELPER_H__F9A9753D_D022_4FC7_B781_DB11A4B4A6E4__INCLUDED_
