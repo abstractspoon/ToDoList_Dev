3adc0b44c097d1b05b998f0ca17cc98a190de14a
abstractspoon <daniel.godson@gmail.com>
2020-06-26 20:45:23 +1000

    Add placeholder form for selecting font

---
+++ Plugins/ImportExport/PDFExporter/PDFExporterCore/PDFExporterPreferencesForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PDFExporter
+{
+	partial class PDFExporterPreferencesForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PDFExporterPreferencesForm));
+			this.btnOK = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnOK
+			// 
+			this.btnOK.Location = new System.Drawing.Point(114, 93);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(75, 23);
+			this.btnOK.TabIndex = 0;
+			this.btnOK.Text = "OK";
+			this.btnOK.UseVisualStyleBackColor = true;
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(195, 93);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 1;
+			this.btnCancel.Text = "Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 14);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(28, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Font";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 30);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(258, 21);
+			this.comboBox1.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 54);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(212, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Note: must support all necessary languages";
+			// 
+			// PDFExporterPreferencesForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(282, 126);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnOK);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "PDFExporterPreferencesForm";
+			this.Text = "Select Content Font";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnOK;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label2;
+	}
+}
--- Plugins/ImportExport/PDFExporter/PDFExporterCore/Properties/Resources.Designer.cs
+++ Plugins/ImportExport/PDFExporter/PDFExporterCore/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace PDFExporter.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon PDFExporter {
+            get {
+                object obj = ResourceManager.GetObject("PDFExporter", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
