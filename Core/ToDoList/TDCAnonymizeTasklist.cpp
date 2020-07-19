215dd5ce4e1d4ea7ad3483f3a4f09b1748e2a2ec
abstractspoon <daniel.godson@gmail.com>
2020-07-19 17:53:25 +1000

    Improve randomisation algorithm (not fully complete)

--- Core/ToDoList/TDCAnonymizeTasklist.cpp
+++ Core/ToDoList/TDCAnonymizeTasklist.cpp
@@ -19,48 +19,132 @@ static char THIS_FILE[]=__FILE__;
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
+CTDCAnonymizeTasklist::CTDCAnonymizeTasklist()
+{
+	// Use a different seed value so that we don't get same
+	// result each time we run this program
+	srand((unsigned int)time(NULL));
+
+}
+
 BOOL CTDCAnonymizeTasklist::Anonymize(LPCTSTR szTaskfile)
 {
+	CString sUnused;
+	return Anonymize(szTaskfile, sUnused);
+}
+
+BOOL CTDCAnonymizeTasklist::Anonymize(LPCTSTR szTaskfile, CString& sAnonFilePath)
+{
 	CTaskFile tasks;
-	
+
 	if (!tasks.Load(szTaskfile))
 		return FALSE;
 
+	if (!CTDCAnonymizeTasklist().AnonymizeTasklist(tasks))
+		return FALSE;
+
+	// else
+	sAnonFilePath = tasks.GetFilePath();
+	return TRUE;
+}
+
+BOOL CTDCAnonymizeTasklist::AnonymizeTasklist(CTaskFile& tasks)
+{
+	if (!BuildContent(tasks))
+		return FALSE;
+
 	// Walk the tasks randomising their titles, comments and string attributes
-	CMapStringToStringMap mapTLD;
-	
-	Anonymize(tasks, NULL, mapTLD);
+	AnonymizeTask(tasks, NULL);
 		
 	// Save to alternate filename
-	CString sFilePath(szTaskfile);
+	CString sFilePath(tasks.GetFilePath());
 	FileMisc::AddToFileName(sFilePath, _T(".rnd"));
 
 	return tasks.Save(sFilePath, SFEF_UTF16);
 }
 
-void CTDCAnonymizeTasklist::Anonymize(CTaskFile& tasks, HTASKITEM hTask, CMapStringToStringMap& mapTLD)
+CString CTDCAnonymizeTasklist::GetContent(const CTaskFile& tasks, HTASKITEM hTask) const
+{
+	CString sContent;
+
+	if (hTask)
+	{
+		TODOITEM tdi;
+		VERIFY(tasks.GetTaskAttributes(hTask, tdi));
+		
+		sContent += tasks.GetTaskTitle(hTask);
+		sContent += tasks.GetTaskComments(hTask);
+	}
+	
+	// Subtasks
+	HTASKITEM hSubTask = tasks.GetFirstTask(hTask);
+	
+	while (hSubTask)
+	{
+		sContent += GetContent(tasks, hSubTask);
+		hSubTask = tasks.GetNextTask(hSubTask);
+	}
+
+	return sContent;
+}
+
+BOOL CTDCAnonymizeTasklist::BuildContent(const CTaskFile& tasks)
+{
+	// Get all task titles and comments
+	m_sContent = GetContent(tasks, NULL);
+
+	// Convert all whitespace and punctuation into spaces
+	int nChar = m_sContent.GetLength();
+
+	while (nChar--)
+	{
+		if (IsWhiteSpaceOrNumberOrPunctuation(m_sContent[nChar]))
+			m_sContent.SetAt(nChar, 32);
+	}
+
+	// Remove all spaces
+	m_sContent.Remove(32);
+
+	return (m_sContent.GetLength() > 0);
+}
+
+BOOL CTDCAnonymizeTasklist::IsWhiteSpaceOrNumberOrPunctuation(TCHAR c)
+{
+	if (c > 127)
+		return FALSE;
+
+	if ((c >= 'A') && (c <= 'Z'))
+		return FALSE;
+
+	if ((c >= 'a') && (c <= 'z'))
+		return FALSE;
+
+	return TRUE;
+}
+
+void CTDCAnonymizeTasklist::AnonymizeTask(CTaskFile& tasks, HTASKITEM hTask)
 {
 	if (hTask)
 	{
 		TODOITEM tdi;
 		VERIFY(tasks.GetTaskAttributes(hTask, tdi));
 		
-		tasks.SetTaskTitle(hTask, Misc::Shuffle(tdi.sTitle));
-		tasks.SetTaskComments(hTask, Misc::Shuffle(tdi.sComments));
+		tasks.SetTaskTitle(hTask, AnonymizeText(tdi.sTitle));
+		tasks.SetTaskComments(hTask, AnonymizeText(tdi.sComments));
 
 		if (!tdi.customComments.IsEmpty())
 			tasks.SetTaskCustomComments(hTask, CBinaryData(_T("__")), tdi.cfComments); // Too hard
 		
-		Anonymize(tdi.aAllocTo,		*mapTLD.GetAddMapping(_T("ALLOCTO")));
-		Anonymize(tdi.aCategories,	*mapTLD.GetAddMapping(_T("CATEGORY")));
-		Anonymize(tdi.aTags,		*mapTLD.GetAddMapping(_T("TAGS")));
+		AnonymizeListItems(tdi.aAllocTo,	*m_mapListData.GetAddMapping(_T("ALLOCTO")));
+		AnonymizeListItems(tdi.aCategories,	*m_mapListData.GetAddMapping(_T("CATEGORY")));
+		AnonymizeListItems(tdi.aTags,		*m_mapListData.GetAddMapping(_T("TAGS")));
 		
-		Anonymize(tdi.sAllocBy,		*mapTLD.GetAddMapping(_T("ALLOCBY")));
-		Anonymize(tdi.sStatus,		*mapTLD.GetAddMapping(_T("STATUS")));
-		Anonymize(tdi.sVersion,		*mapTLD.GetAddMapping(_T("VERSION")));
+		AnonymizeListItem(tdi.sAllocBy,		*m_mapListData.GetAddMapping(_T("ALLOCBY")));
+		AnonymizeListItem(tdi.sStatus,		*m_mapListData.GetAddMapping(_T("STATUS")));
+		AnonymizeListItem(tdi.sVersion,		*m_mapListData.GetAddMapping(_T("VERSION")));
 		
-		Anonymize(tdi.sExternalID,	*mapTLD.GetAddMapping(_T("EXTERNALID")));
-		Anonymize(tdi.sCreatedBy,	*mapTLD.GetAddMapping(_T("CREATEDBY")));
+		AnonymizeListItem(tdi.sExternalID,	*m_mapListData.GetAddMapping(_T("EXTERNALID")));
+		AnonymizeListItem(tdi.sCreatedBy,	*m_mapListData.GetAddMapping(_T("CREATEDBY")));
 		
 		VERIFY(tasks.SetTaskAllocatedTo(hTask, tdi.aAllocTo));
 		VERIFY(tasks.SetTaskCategories(hTask, tdi.aCategories));
@@ -77,21 +161,21 @@ void CTDCAnonymizeTasklist::Anonymize(CTaskFile& tasks, HTASKITEM hTask, CMapStr
 	
 	while (hSubTask)
 	{
-		Anonymize(tasks, hSubTask, mapTLD);
+		AnonymizeTask(tasks, hSubTask);
 		
 		hSubTask = tasks.GetNextTask(hSubTask);
 	}
 }
 
-void CTDCAnonymizeTasklist::Anonymize(CStringArray& aItems, CMapStringToString& mapTLD)
+void CTDCAnonymizeTasklist::AnonymizeListItems(CStringArray& aItems, CMapStringToString& mapTLD)
 {
 	int nItem = aItems.GetSize();
 	
 	while (nItem--)
-		Anonymize(aItems[nItem], mapTLD);
+		AnonymizeListItem(aItems[nItem], mapTLD);
 }
 
-void CTDCAnonymizeTasklist::Anonymize(CString& sItem, CMapStringToString& mapTLD)
+void CTDCAnonymizeTasklist::AnonymizeListItem(CString& sItem, CMapStringToString& mapTLD)
 {
 	if (!sItem.IsEmpty())
 	{
@@ -100,7 +184,7 @@ void CTDCAnonymizeTasklist::Anonymize(CString& sItem, CMapStringToString& mapTLD
 		if (!mapTLD.Lookup(sItem, sRandom))
 		{
 			// add to map
-			sRandom = Misc::Shuffle(sItem);
+			sRandom = AnonymizeText(sItem);
 			mapTLD[sItem] = sRandom;
 		}
 		
@@ -108,4 +192,29 @@ void CTDCAnonymizeTasklist::Anonymize(CString& sItem, CMapStringToString& mapTLD
 		ASSERT(!sItem.IsEmpty());
 	}
 }
+
+CString CTDCAnonymizeTasklist::AnonymizeText(const CString& sItem)
+{
+	CString sRandom(sItem);
+	int nChar = sRandom.GetLength();
+
+	while (nChar--)
+	{
+		TCHAR c = sRandom[nChar], cRnd = c;
+
+		if (!IsWhiteSpaceOrNumberOrPunctuation(c))
+		{
+			while (cRnd == c)
+			{
+				int nRnd = (rand() % (m_sContent.GetLength() + 1));
+				cRnd = m_sContent[nRnd];
+			}
+		}
+
+		sRandom.SetAt(nChar, cRnd);
+	}
+
+	return sRandom;
+}
+
 
--- Core/ToDoList/TDCAnonymizeTasklist.h
+++ Core/ToDoList/TDCAnonymizeTasklist.h
@@ -21,11 +21,26 @@ class CTDCAnonymizeTasklist
 {
 public:
 	static BOOL Anonymize(LPCTSTR szTaskfile);
+	static BOOL Anonymize(LPCTSTR szTaskfile, CString& sAnonFilePath);
 
 protected:
-	static void Anonymize(CTaskFile& tasks, HTASKITEM hTask, CMapStringToStringMap& mapTLD);
-	static void Anonymize(CStringArray& aItems, CMapStringToString& mapTLD);
-	static void Anonymize(CString& sItem, CMapStringToString& mapTLD);
+	CString m_sContent;
+	CMapStringToStringMap m_mapListData;
+
+protected:
+	CTDCAnonymizeTasklist();
+
+	BOOL AnonymizeTasklist(CTaskFile& tasks);
+	void AnonymizeTask(CTaskFile& tasks, HTASKITEM hTask);
+	void AnonymizeListItems(CStringArray& aItems, CMapStringToString& mapTLD);
+	void AnonymizeListItem(CString& sItem, CMapStringToString& mapTLD);
+	CString AnonymizeText(const CString& sItem);
+
+	BOOL BuildContent(const CTaskFile& tasks);
+	CString GetContent(const CTaskFile& tasks, HTASKITEM hTask) const;
+
+	static BOOL IsWhiteSpaceOrNumberOrPunctuation(TCHAR c);
+
 };
 
 #endif // !defined(AFX_TDCANONYMIZETASKLIST_H__36BFC958_99EA_4B0B_8E44_04AB78F47CE4__INCLUDED_)
