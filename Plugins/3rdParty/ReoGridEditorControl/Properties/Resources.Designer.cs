137936d2f3d5627c1e1af09ffb0431dfee2a34d1
abstractspoon <daniel.godson@gmail.com>
2020-08-24 17:53:58 +1000

    Add placeholder image for rendered toolbar buttons

--- Plugins/3rdParty/ReoGridEditorControl/Properties/Resources.Designer.cs
+++ Plugins/3rdParty/ReoGridEditorControl/Properties/Resources.Designer.cs
@@ -929,6 +929,16 @@ namespace unvell.ReoGrid.Editor.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap placeholder {
+            get {
+                object obj = ResourceManager.GetObject("placeholder", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap PortraitHH {
             get {
                 object obj = ResourceManager.GetObject("PortraitHH", resourceCulture);
