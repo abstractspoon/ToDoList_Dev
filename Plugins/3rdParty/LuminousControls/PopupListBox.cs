1209779c40e20ef308ca2c19752e2c9176eb6508
abstractspoon <daniel.godson@gmail.com>
2021-03-28 18:47:59 +1100

    Move class to 3rd Party project

---
+++ Plugins/3rdParty/LuminousControls/PopupListBox.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+//////////////////////////////////////////////////////////////////////
+
+namespace Luminous.Windows.Forms
+{
+
+	public class PopupListBox : Popup
+	{
+		class TrackingListBox : ListBox
+		{
+			public TrackingListBox()
+			{
+				m_LastMouseMove = PointToClient(MousePosition);
+			}
+
+			Point m_LastMouseMove;
+
+			protected override void OnMouseMove(MouseEventArgs e)
+			{
+				if (m_LastMouseMove != e.Location)
+				{
+					m_LastMouseMove = e.Location;
+
+					int hit = IndexFromPoint(e.Location);
+
+					if (hit != -1)
+						SelectedIndex = hit;
+				}
+			}
+		}
+
+		// --------------------------------------------------------------
+
+		public PopupListBox() : base(new TrackingListBox())
+		{
+			ListBox.MouseDown += new MouseEventHandler(OnListMouseDown);
+			ListBox.KeyDown += new KeyEventHandler(OnListKeyDown);
+		}
+
+		public PopupListBox(object[] items) : this()
+		{
+			ListBox.Items.AddRange(items);
+			ListBox.SelectedIndex = 0;
+		}
+
+		public ListBox ListBox
+		{
+			get { return base.Content as ListBox; }
+		}
+
+		protected void OnListMouseDown(object sender, MouseEventArgs e)
+		{
+			// We receive a bogus mouse-down immediately 
+			// after showing so we check it first
+			Point mousePos = MousePosition;
+			mousePos = ListBox.PointToClient(mousePos);
+
+			if (e.Location == mousePos)
+			{
+				int hit = ListBox.IndexFromPoint(e.Location);
+
+				if (hit != -1)
+				{
+					Close(ToolStripDropDownCloseReason.ItemClicked);
+				}
+			}
+		}
+
+		protected void OnListKeyDown(object sender, KeyEventArgs e)
+		{
+			if ((e.KeyCode == Keys.Return) && (ListBox.SelectedIndex != -1))
+			{
+				Close(ToolStripDropDownCloseReason.ItemClicked);
+			}
+		}
+	}
+
+}
--- Plugins/ContentControl/HTMLContentControl/HTMLContentControlCore/TDLHtmlEditorControl.cs
+++ Plugins/ContentControl/HTMLContentControl/HTMLContentControlCore/TDLHtmlEditorControl.cs
@@ -8,6 +8,7 @@ using System.Runtime.InteropServices;
 
 using Abstractspoon.Tdl.PluginHelpers;
 using Command.Handling;
+using Luminous.Windows.Forms;
 
 //////////////////////////////////////////////////////////////////////
 
@@ -33,79 +34,6 @@ namespace HTMLContentControl
 
 		public Task.Attribute attrib;
 		public List<String> values;
-	}
-
-	// --------------------------------------------------------------
-
-	class PopupListBox : Luminous.Windows.Forms.Popup
-	{
-		class TrackingListBox : ListBox
-		{
-			public TrackingListBox()
-			{
-				m_LastMouseMove = PointToClient(MousePosition);
-			}
-
-			Point m_LastMouseMove;
-
-			protected override void OnMouseMove(MouseEventArgs e)
-			{
-				if (m_LastMouseMove != e.Location)
-				{
-					m_LastMouseMove = e.Location;
-
-					int hit = IndexFromPoint(e.Location);
-
-					if (hit != -1)
-						SelectedIndex = hit;
-				}
-			}
-		}
-
-		// --------------------------------------------------------------
-
-		public PopupListBox() : base(new TrackingListBox())
-		{
-			ListBox.MouseDown += new MouseEventHandler(OnListMouseDown);
-			ListBox.KeyDown += new KeyEventHandler(OnListKeyDown);
-		}
-
-		public PopupListBox(object[] items) : this()
-		{
-			ListBox.Items.AddRange(items);
-			ListBox.SelectedIndex = 0;
-		}
-
-		public ListBox ListBox
-		{
-			get { return base.Content as ListBox; }
-		}
-
-		protected void OnListMouseDown(object sender, MouseEventArgs e)
-		{
-			// We receive a bogus mouse-down immediately 
-			// after showing so we check it first
-			Point mousePos = MousePosition;
-			mousePos = ListBox.PointToClient(mousePos);
-
-			if (e.Location == mousePos)
-			{
-				int hit = ListBox.IndexFromPoint(e.Location);
-
-				if (hit != -1)
-				{
-					Close(ToolStripDropDownCloseReason.ItemClicked);
-				}
-			}
-		}
-
-		protected void OnListKeyDown(object sender, KeyEventArgs e)
-		{
-			if ((e.KeyCode == Keys.Return) && (ListBox.SelectedIndex != -1))
-			{
-				Close(ToolStripDropDownCloseReason.ItemClicked);
-			}
-		}
 	}
 
 	// --------------------------------------------------------------
@@ -429,7 +357,7 @@ namespace HTMLContentControl
 						var logFont = new unvell.Common.Win32Lib.Win32.LOGFONT();
 						font.ToLogFont(logFont);
 
-						PopupListBox popup = new PopupListBox(args.values.ToArray());
+						var popup = new PopupListBox(args.values.ToArray());
 						popup.Closed += new ToolStripDropDownClosedEventHandler(OnAttributeListBoxClosed);
 
 						pos.Offset(0, popup.Height + Math.Abs(logFont.lfHeight));
