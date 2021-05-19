b18a2d22913827eb958e4c9bed66aa517e9457dd
abstractspoon <daniel.godson@gmail.com>
2021-05-19 19:36:55 +1000

    Fix paths for release builds

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -17,21 +17,20 @@ pause
 REM - Build core app in VC6
 cd %REPO%\Core
 
+REM - Detours
 "C:\Program Files (x86)\Microsoft Visual Studio\Common\MSDev98\Bin\msdev.exe" .\3rdParty\Detours\Detours.dsw /MAKE "ALL - Win32 Unicode Release"
 
+REM - Core app
 "C:\Program Files (x86)\Microsoft Visual Studio\Common\MSDev98\Bin\msdev.exe" .\ToDoList_Core.dsw /MAKE "ALL - Win32 Unicode Release" 
 
 REM - Build plugins using MSBuild for reliability
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
 SET BUILDPARAMS=/t:Build /p:Configuration=Release /m /v:normal /noWarn:MSB3267;MSB3305;LNK4248;CS1762;LNK4221;MSB3026
+
+REM - This is required to get the warning suppression to work
 SET VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140\
 
-REM - Build ToDoList_Core libraries in VS2010 for linking into PluginsHelpers
 cd %REPO%\Plugins
-
-%MSBUILD% .\Core\ToDoList_Core_For_Plugins.sln %BUILDPARAMS%
-
-REM - Build rest of plugins
 %MSBUILD% .\ToDoList_Plugins.sln %BUILDPARAMS%
 
 REM - Allow caller to cancel building Zip
