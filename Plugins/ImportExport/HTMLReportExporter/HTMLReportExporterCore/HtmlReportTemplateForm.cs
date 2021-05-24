3a550247b29a24e0d1dc05519f4fc56b1dc8d286
abstractspoon <daniel.godson@gmail.com>
2021-05-24 12:36:07 +1000

    Initialise image panel with user's last choice

--- Plugins/ImportExport/HTMLReportExporter/HTMLReportExporterCore/HtmlReportTemplateForm.cs
+++ Plugins/ImportExport/HTMLReportExporter/HTMLReportExporterCore/HtmlReportTemplateForm.cs
@@ -708,6 +708,8 @@ namespace HTMLReportExporter
 
 			dialog.Icon = HTMLReportExporter.Properties.Resources.HTMLReporter;
 			dialog.ShowIcon = true;
+			dialog.ImageLink = m_Template.BackImage;
+			dialog.Text = this.toolStripInsertImage.ToolTipText;// m_Trans.Translate("Set Background Image");
 
 			m_Trans.Translate(dialog, dialog.Tooltip);
 
