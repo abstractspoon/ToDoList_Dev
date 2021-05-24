afa9bbb042fd4311b7830df810fd46627364e0dd
abstractspoon <daniel.godson@gmail.com>
2021-05-24 12:35:17 +1000

    Disable backgroud image repeating

--- Plugins/3rdParty/MSHtmlEditorControl/HtmlEditorControl.cs
+++ Plugins/3rdParty/MSHtmlEditorControl/HtmlEditorControl.cs
@@ -1589,6 +1589,8 @@ namespace MSDN.Html.Editor
 						bodyStyle.backgroundImage = String.Empty;
 					else
 						bodyStyle.backgroundImage = String.Format("url({0})", value);
+
+					bodyStyle.backgroundRepeat = "no-repeat";
 				}
             }
 
