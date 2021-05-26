ef90a672dd108046d2c35fc4952bc241d90b4af7
abstractspoon <daniel.godson@gmail.com>
2021-05-26 17:58:37 +1000

    Create demo project for exploring iTextSharp

---
+++ Plugins/3rdParty/iTextDemo/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace iTextDemo
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textHtmlInput = new System.Windows.Forms.TextBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.webBrowserPDF = new System.Windows.Forms.WebBrowser();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnParse = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textHtmlInput
+			// 
+			this.textHtmlInput.Location = new System.Drawing.Point(13, 26);
+			this.textHtmlInput.Name = "textHtmlInput";
+			this.textHtmlInput.Size = new System.Drawing.Size(369, 20);
+			this.textHtmlInput.TabIndex = 0;
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Location = new System.Drawing.Point(388, 24);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+			this.btnBrowse.TabIndex = 1;
+			this.btnBrowse.Text = "Browse";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.OnBrowseInputFile);
+			// 
+			// webBrowserPDF
+			// 
+			this.webBrowserPDF.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.webBrowserPDF.Location = new System.Drawing.Point(15, 78);
+			this.webBrowserPDF.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowserPDF.Name = "webBrowserPDF";
+			this.webBrowserPDF.Size = new System.Drawing.Size(530, 343);
+			this.webBrowserPDF.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(10, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(64, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "HTML Input";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(10, 62);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(63, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "PDF Output";
+			// 
+			// btnParse
+			// 
+			this.btnParse.Location = new System.Drawing.Point(470, 24);
+			this.btnParse.Name = "btnParse";
+			this.btnParse.Size = new System.Drawing.Size(75, 23);
+			this.btnParse.TabIndex = 5;
+			this.btnParse.Text = "Reparse";
+			this.btnParse.UseVisualStyleBackColor = true;
+			this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(558, 433);
+			this.Controls.Add(this.btnParse);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.webBrowserPDF);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.textHtmlInput);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textHtmlInput;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.WebBrowser webBrowserPDF;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnParse;
+	}
+}
+
---
+++ Plugins/3rdParty/iTextDemo/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Win32;
+
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.html;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+
+namespace iTextDemo
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\AbstractSpoon\iTextDemo");
+
+			if (key != null)
+			{
+				textHtmlInput.Text = key.GetValue("HTMLInputFile", "").ToString();
+
+				CreatePDF(textHtmlInput.Text);
+			}
+		}
+
+		protected override void OnClosed(EventArgs e)
+		{
+			RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\AbstractSpoon\iTextDemo");
+
+			if (key != null)
+				key.SetValue("HTMLInputFile", textHtmlInput.Text);
+
+			base.OnClosed(e);
+		}
+
+		private void OnBrowseInputFile(object sender, EventArgs e)
+		{
+			var dlg = new OpenFileDialog
+			{
+				Title = this.Text,
+
+				AutoUpgradeEnabled = true,
+				CheckFileExists = true,
+				CheckPathExists = true,
+				Filter = "HTML Files (*.htm, *.html)|*.htm;*.html",
+				FilterIndex = 0,
+				FileName = textHtmlInput.Text
+			};
+
+			if (dlg.ShowDialog() == DialogResult.OK)
+			{
+				textHtmlInput.Text = dlg.FileName;
+
+				CreatePDF(textHtmlInput.Text);
+			}
+		}
+
+		private void CreatePDF(string htmlInputFile)
+		{
+			webBrowserPDF.Navigate("about:blank");
+
+			if (string.IsNullOrEmpty(htmlInputFile))
+				return;
+
+			try
+			{
+				MemoryStream workStream = new MemoryStream();
+				Document pdfDoc = new Document(PageSize.A4);
+				PdfWriter pdfWriter = PdfWriter.GetInstance(pdfDoc, workStream);
+				pdfWriter.CloseStream = false;
+//				pdfWriter.PageEvent = new PDFBackgroundImage(m_WatermarkImagePath);
+
+				pdfDoc.Open();
+
+				string htmlInput = File.ReadAllText(htmlInputFile);
+
+				//string FixBrokenMarkup(string broken)
+				var h = new HtmlAgilityPack.HtmlDocument()
+				{
+					OptionAutoCloseOnEnd = true,
+					OptionFixNestedTags = true,
+					OptionWriteEmptyNodes = true
+				};
+				h.LoadHtml(htmlInput);
+
+				// UPDATED to remove HtmlCommentNode
+				var comments = h.DocumentNode.SelectNodes("//comment()");
+				if (comments != null)
+				{
+					foreach (var node in comments)
+						node.Remove();
+				}
+
+				htmlInput = h.DocumentNode.SelectNodes("child::*") != null
+						//                            ^^^^^^^^^^
+						// XPath above: string plain-text or contains markup/tags
+						? h.DocumentNode.WriteTo()
+						: string.Format("<span>{0}</span>", htmlInput);
+
+// 				using (var sr = new StringReader(htmlInput))
+// 				{
+// 					//Parse the HTML
+// 					//htmlWorker.Parse(sr);
+// 					var parser = iTextSharp.tool.xml.XMLWorkerHelper.GetInstance();
+// 					parser.ParseXHtml(pdfWriter, pdfDoc, sr);
+// 				}
+
+				Section section = new Chapter(0);
+				AddContent(htmlInput, section);
+				
+				pdfDoc.Add(section);
+				pdfDoc.Close();
+
+				byte[] byteInfo = workStream.ToArray();
+				workStream.Write(byteInfo, 0, byteInfo.Length);
+				workStream.Position = 0;
+
+				string pdfOutputFile = Path.ChangeExtension(htmlInputFile, "pdf");
+
+				File.WriteAllBytes(pdfOutputFile, byteInfo);
+
+				Uri pdfLink;
+
+				if (Uri.TryCreate(pdfOutputFile, UriKind.Absolute, out pdfLink))
+					webBrowserPDF.Navigate(pdfLink);
+			}
+			catch (Exception e)
+			{
+			}
+		}
+
+		private void AddContent(string html, Section section)
+		{
+			var elements = iTextSharp.tool.xml.XMLWorkerHelper.ParseToElementList(html, "");
+			//var elements = HTMLWorker.ParseToList(new StringReader(html), new StyleSheet()/*m_ParseStyles*/);
+
+			foreach (IElement element in elements)
+			{
+				var elementType = element.ToString();
+
+				section.Add(element);
+/*
+//				if (element.IsContent())
+				{
+					var para = new Paragraph();
+
+					foreach (var obj in element.Chunks)
+					{
+						var chunk = (obj as Chunk);
+						var objType = obj.ToString();
+
+						if (chunk != null)
+						{
+// 							var font = chunk.Font;
+// 
+// 							if (font == null)
+// 								font = new Font(m_BaseFont, m_BaseFontSize);
+// 							else
+// 								font = new Font(m_BaseFont, font.Size, font.Style, font.Color);
+// 
+// 							chunk.Font = font;
+							para.Add(chunk);
+						}
+						else
+						{
+							para.Add(obj);
+						}
+					}
+
+					section.Add(para);
+				}
+// 				else
+// 				{
+// 					section.Add(element);
+// 				}
+*/
+			}
+		}
+
+		private void btnParse_Click(object sender, EventArgs e)
+		{
+			CreatePDF(textHtmlInput.Text);
+		}
+	}
+}
