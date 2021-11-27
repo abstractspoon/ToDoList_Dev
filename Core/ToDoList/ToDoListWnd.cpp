c491d4bd065b3cc4baa62b0c110afbff5e2f26fc
abstractspoon <daniel.godson@gmail.com>
2021-11-27 19:24:26 +1100

    Merge filter fix from ToDoList_8.1

--- Core/ToDoList/ToDoListWnd.cpp
+++ Core/ToDoList/ToDoListWnd.cpp
@@ -11473,9 +11473,13 @@ void CToDoListWnd::OnChangeFilter(TDCFILTER& filter, const CString& sCustom, DWO
 
 		tdc.SetAdvancedFilter(filter);
 	}
-	else
+	else if (filter.IsSet())
 	{
 		tdc.SetFilter(filter);
+	}
+	else
+	{
+		tdc.ClearFilter();
 	}
 
 	if (bUpdateFilterCtrls)
