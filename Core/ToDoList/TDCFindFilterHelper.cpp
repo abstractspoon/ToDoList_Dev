a67cf0b679636fedb240960ad5babb9f121d0d3d
abstractspoon <daniel.godson@gmail.com>
2023-02-28 10:34:17 +1100

    Add placeholder helper class

---
+++ Core/ToDoList/TDCFindFilterHelper.cpp
@@ -0,0 +1,23 @@
+// ToDoCtrl.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "TDCFindFilterHelper.h"
+
+//#include "..\Shared\AutoFlag.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+
+CTDCFindFilterHelper::CTDCFindFilterHelper() 
+{
+}
+
+CTDCFindFilterHelper::~CTDCFindFilterHelper()
+{
+}
---
+++ Core/ToDoList/TDCFindFilterHelper.h
@@ -0,0 +1,34 @@
+#if !defined(AFX_TDCFINDFILTERHELPER_H__5951FDE6_508A_4A9D_A55D_D16EB026AEF7__INCLUDED_)
+#define AFX_TDCFINDFILTERHELPER_H__5951FDE6_508A_4A9D_A55D_D16EB026AEF7__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// ToDoCtrl.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+
+// class CToDoCtrl;
+// class CPreferences;
+
+/////////////////////////////////////////////////////////////////////////////
+// 
+
+class CTDCFindFilterHelper
+{
+// Construction
+public:
+	CTDCFindFilterHelper();
+	virtual ~CTDCFindFilterHelper();
+
+protected:
+
+	// Implementation
+protected:
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_TDCFINDFILTERHELPER_H__5951FDE6_508A_4A9D_A55D_D16EB026AEF7__INCLUDED_)
