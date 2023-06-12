5ae1c0d83e491328655c69d29190a8f2a11ab571
abstractspoon <daniel.godson@gmail.com>
2023-06-12 13:18:09 +1000

    Refactor array functions to use a macro

--- Core/KanbanBoard/KanbanColumnCtrlArray.h
+++ Core/KanbanBoard/KanbanColumnCtrlArray.h
@@ -56,6 +56,7 @@ public:
 	void Sort();
 	void Sort(TDC_ATTRIBUTE nBy, BOOL bAscending);
 	void GroupBy(TDC_ATTRIBUTE nAttrib, BOOL bAscending);
+	void SetGroupHeaderBackgroundColor(COLORREF color);
 
 	void SetSelectedColumn(const CKanbanColumnCtrl* pSelList);
 	void SetDropTarget(const CKanbanColumnCtrl* pTarget);
