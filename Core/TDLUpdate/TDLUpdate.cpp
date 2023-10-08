127f7feb9d2a969993e5870a51e40a7f51cf6393
abstractspoon <daniel.godson@gmail.com>
2023-10-08 18:25:29 +1100

    Add Dark Mode to updater

--- Core/TDLUpdate/TDLUpdate.cpp
+++ Core/TDLUpdate/TDLUpdate.cpp
@@ -14,6 +14,7 @@
 #include "..\shared\misc.h"
 #include "..\shared\rtlstylemgr.h"
 #include "..\shared\OSVersion.h"
+#include "..\shared\DarkMode.h"
 
 #include "..\todolist\tdcswitch.h"
 
@@ -60,92 +61,98 @@ BOOL CTDLUpdateApp::InitInstance()
 	ParseCommandLine(cmdInfo);
 
 	// must have App ID
-	if (cmdInfo.GetOption(SWITCH_APPID) == TDLAPPID)
-	{
-		// RTL support
-		if (cmdInfo.HasOption(SWITCH_RTL))
-			CRTLStyleMgr::Initialize();
+	if (cmdInfo.GetOption(SWITCH_APPID) != TDLAPPID)
+		return FALSE;
 
-		// position for more than one screen
-		CPoint ptPos(_ttol(cmdInfo.GetOption(SWITCH_POSITION)));
-		BOOL bPreRelease = cmdInfo.HasOption(SWITCH_PRERELEASE);
-		BOOL bRestartElevated = cmdInfo.HasOption(SWITCH_RESTARTELEVATED);
+	// Inherited commandline options
+	if (cmdInfo.HasOption(SWITCH_DARKMODE))
+		CDarkMode::Enable();
+
+	if (cmdInfo.HasOption(SWITCH_RTL))
+		CRTLStyleMgr::Initialize();
+
+	// position for more than one screen
+	CPoint ptPos(_ttol(cmdInfo.GetOption(SWITCH_POSITION)));
+	BOOL bPreRelease = cmdInfo.HasOption(SWITCH_PRERELEASE);
+	BOOL bRestartElevated = cmdInfo.HasOption(SWITCH_RESTARTELEVATED);
 
 #ifdef _DEBUG
-		// check if TDL just wants to see the ui
-		if (cmdInfo.HasOption(SWITCH_SHOWUI))
-		{
-			CTDLWebUpdateProgressDlg dialog(ptPos);
-			dialog.DoModal();
+	// check if TDL just wants to see the ui
+	if (cmdInfo.HasOption(SWITCH_SHOWUI))
+	{
+		CTDLWebUpdateProgressDlg dialog(ptPos);
+		dialog.DoModal();
 
-			return FALSE;
-		}
-		
-		// Or just wants to test the download link
-		if (cmdInfo.HasOption(SWITCH_TESTDOWNLOAD))
-		{
-			CTDLWebUpdater wu(ptPos, bPreRelease);
-			wu.DoUpdate(_T(""), _T(""), FALSE, TRUE);
-			
-			return FALSE;
-		}
+		return FALSE;
+	}
+
+	// Or just wants to test the download link
+	if (cmdInfo.HasOption(SWITCH_TESTDOWNLOAD))
+	{
+		CTDLWebUpdater wu(ptPos, bPreRelease);
+		wu.DoUpdate(_T(""), _T(""), FALSE, TRUE);
+
+		return FALSE;
+	}
 #endif
 
-		CString sLangFile = cmdInfo.GetOption(SWITCH_LANG);
-		
-		// if the app folder is not specified then we copy ourselves
-		// to the temp directory and run from there
-		CString sAppFolder;
-		
-		if (cmdInfo.GetOption(SWITCH_APPFOLDER, sAppFolder) && !sAppFolder.IsEmpty())
+	CString sLangFile = cmdInfo.GetOption(SWITCH_LANG);
+
+	// if the app folder is not specified then we copy ourselves
+	// to the temp directory and run from there
+	CString sAppFolder;
+
+	if (cmdInfo.GetOption(SWITCH_APPFOLDER, sAppFolder) && !sAppFolder.IsEmpty())
+	{
+		// initialize translator
+		if (!sLangFile.IsEmpty())
+			CLocalizer::Initialize(sLangFile, ITTTO_TRANSLATEONLY);
+
+		// previous commandline is encoded
+		CString sPrevCmdLine = Base64Coder::Decode(cmdInfo.GetOption(SWITCH_CMDLINE));
+
+		// do the update
+		DoUpdate(sAppFolder, sPrevCmdLine, ptPos, bPreRelease, bRestartElevated);
+
+		CLocalizer::Release();
+	}
+	else
+	{
+		CString sAppPath = FileMisc::GetAppFilePath();
+		CString sTempAppPath = FileMisc::GetTempFilePath(_T("TDLUpdate"), _T("exe"));
+
+		if (FileMisc::CopyFile(sAppPath, sTempAppPath, TRUE, TRUE))
 		{
-			// initialize translator
+			CEnCommandLineInfo params;
+
+			params.SetOption(SWITCH_APPID, TDLAPPID);
+			params.SetOption(SWITCH_APPFOLDER, FileMisc::GetAppFolder());
+			params.SetOption(SWITCH_CMDLINE, cmdInfo.GetOption(SWITCH_CMDLINE));
+			params.SetOption(SWITCH_POSITION, cmdInfo.GetOption(SWITCH_POSITION));
+
+			if (CRTLStyleMgr::IsRTL())
+				params.SetOption(SWITCH_RTL);
+	
+			if (CDarkMode::IsEnabled())
+				params.SetOption(SWITCH_DARKMODE);
+
 			if (!sLangFile.IsEmpty())
-				CLocalizer::Initialize(sLangFile, ITTTO_TRANSLATEONLY);
-			
-			// previous commandline is encoded
-			CString sPrevCmdLine = Base64Coder::Decode(cmdInfo.GetOption(SWITCH_CMDLINE));
-			
-			// do the update
-			DoUpdate(sAppFolder, sPrevCmdLine, ptPos, bPreRelease, bRestartElevated);
-			
-			CLocalizer::Release();
-		}
-		else
-		{
-			CString sAppPath = FileMisc::GetAppFilePath();
-			CString sTempAppPath = FileMisc::GetTempFilePath(_T("TDLUpdate"), _T("exe"));
-			
-			if (FileMisc::CopyFile(sAppPath, sTempAppPath, TRUE, TRUE))
 			{
-				CEnCommandLineInfo params;
-				
-				params.SetOption(SWITCH_APPID, TDLAPPID);
-				params.SetOption(SWITCH_APPFOLDER, FileMisc::GetAppFolder());
-				params.SetOption(SWITCH_CMDLINE, cmdInfo.GetOption(SWITCH_CMDLINE));
-				params.SetOption(SWITCH_POSITION, cmdInfo.GetOption(SWITCH_POSITION));
-				
-				if (CRTLStyleMgr::IsRTL())
-					params.SetOption(SWITCH_RTL);
-				
-				if (!sLangFile.IsEmpty())
-				{
-					// copy TransText.dll also
-					CString sTTPath = FileMisc::GetAppFolder() + _T("\\TransText.dll");
-					CString sTempTTPath = FileMisc::GetTempFilePath(_T("TransText"), _T("dll"));
-					
-					// note: failure to copy just means no translation
-					if (FileMisc::CopyFile(sTTPath, sTempTTPath, TRUE, TRUE))
-						params.SetOption(SWITCH_LANG, sLangFile);
-				}
-				
-				if (bPreRelease)
-					params.SetOption(SWITCH_PRERELEASE);
-				
-				if (FileMisc::Run(NULL, sTempAppPath, params.GetCommandLine()) < SE_ERR_SUCCESS)
-				{
-					// TODO
-				}
+				// copy TransText.dll also
+				CString sTTPath = FileMisc::GetAppFolder() + _T("\\TransText.dll");
+				CString sTempTTPath = FileMisc::GetTempFilePath(_T("TransText"), _T("dll"));
+
+				// note: failure to copy just means no translation
+				if (FileMisc::CopyFile(sTTPath, sTempTTPath, TRUE, TRUE))
+					params.SetOption(SWITCH_LANG, sLangFile);
+			}
+
+			if (bPreRelease)
+				params.SetOption(SWITCH_PRERELEASE);
+
+			if (FileMisc::Run(NULL, sTempAppPath, params.GetCommandLine()) < SE_ERR_SUCCESS)
+			{
+				// TODO
 			}
 		}
 	}
