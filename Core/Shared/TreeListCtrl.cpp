ce79d25d8f69fecfb184b10d4cbdd4d76cb8d7a4
abstractspoon <daniel.godson@gmail.com>
2023-12-12 17:17:47 +1100

    Fix rendering artifacts using keyboard shortcuts to move selection

--- Core/Shared/TreeListCtrl.cpp
+++ Core/Shared/TreeListCtrl.cpp
@@ -511,8 +511,9 @@ BOOL CTreeListCtrl::SelectItem(HTREEITEM hti)
 	// Don't allow any horizontal movement because this 
 	// will break the way we have implemented click-handling
 	{
+		CHoldRedraw hr(*this);
 		CHoldHScroll hs(m_tree);
-		CTLSHoldResync hr(*this); 
+		CTLSHoldResync hr2(*this); 
 
 		SelectTreeItem(hti, FALSE);
 	}
