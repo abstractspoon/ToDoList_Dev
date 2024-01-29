005cf4ec78efe18d6d37ef60adc2242596aea090
abstractspoon <daniel.godson@gmail.com>
2024-01-29 20:00:24 +1100

    Fix multiline support

--- Core/3rdParty/StatusBarACT.cpp
+++ Core/3rdParty/StatusBarACT.cpp
@@ -98,7 +98,7 @@ int CStatusBarACT::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	SetBarStyle(GetBarStyle() & ~CBRS_TOOLTIPS);
 	
 	// allow '\n' chars to wrap tips
-	m_tooltip.SetMaxTipWidth(-1);
+	m_tooltip.SetMaxTipWidth(SHRT_MAX);
 	
 	return 0;
 }
