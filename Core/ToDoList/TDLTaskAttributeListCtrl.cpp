01b64689bdea67e727177d55e9843d20258c5d58
abstractspoon <daniel.godson@gmail.com>
2024-02-08 16:03:49 +1100

    Fix disabled cell text under 'Dark Mode'

--- Core/ToDoList/TDLTaskAttributeListCtrl.cpp
+++ Core/ToDoList/TDLTaskAttributeListCtrl.cpp
@@ -819,7 +819,7 @@ COLORREF CTDLTaskAttributeListCtrl::GetItemBackColor(int nItem, int nCol, BOOL b
 COLORREF CTDLTaskAttributeListCtrl::GetItemTextColor(int nItem, int nCol, BOOL bSelected, BOOL bDropHighlighted, BOOL bWndFocus) const
 {
 	if (!CanEditCell(nItem, VALUE_COL))
-		return GetSysColor(COLOR_GRAYTEXT);
+		return GetSysColor(COLOR_3DSHADOW);
 
 	if (nCol == VALUE_COL)
 	{
