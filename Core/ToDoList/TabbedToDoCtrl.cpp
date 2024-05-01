8f365950a1ad74b1a0f458033bb743d23e40dd6b
abstractspoon <daniel.godson@gmail.com>
2024-05-01 20:37:28 +1000

    Fix typo

--- Core/ToDoList/TabbedToDoCtrl.cpp
+++ Core/ToDoList/TabbedToDoCtrl.cpp
@@ -2635,7 +2635,7 @@ int CTabbedToDoCtrl::GetSelectedTaskIDs(CDWordArray& aTaskIDs, BOOL bTrue) const
 int CTabbedToDoCtrl::GetSelectedTaskIDs(CDWordArray& aTaskIDs, DWORD& dwFocusedTaskID, BOOL bRemoveChildDupes) const
 {
 	if (InListView())
-		return m_taskList.GetSelectedTaskIDs(aTaskIDs, dwFocusedTaskID, bRemoveChildDupes);
+		return m_taskList.GetSelectedTaskIDs(aTaskIDs, dwFocusedTaskID);
 
 	// else
 	return CToDoCtrl::GetSelectedTaskIDs(aTaskIDs, dwFocusedTaskID, bRemoveChildDupes);
