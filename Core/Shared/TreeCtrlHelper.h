a0a50c7b36f8eca49ce6612d717d53d0d0d2d6da
abstractspoon <daniel.godson@gmail.com>
2024-06-30 19:51:35 +1000

    Fix argument spelling

--- Core/Shared/TreeCtrlHelper.h
+++ Core/Shared/TreeCtrlHelper.h
@@ -112,7 +112,7 @@ public:
 	BOOL ItemHasParent(HTREEITEM hti, HTREEITEM htiParent) const;
 	BOOL IsAnyItemExpanded() const;
 	BOOL IsAnyItemCollapsed() const;
-	int GetItemData(CDWordArray& aItemData, BOOL bIncParents = TRUE, BOOL bInCollapsedChildren = TRUE) const;
+	int GetItemData(CDWordArray& aItemData, BOOL bIncParents = TRUE, BOOL bIncCollapsedChildren = TRUE) const;
 
 	BOOL IsItemFullyVisible(HTREEITEM hti) const;
 	BOOL IsItemVisible(HTREEITEM hti, BOOL bVertPartialOK = TRUE, BOOL bHorzPartialOK = TRUE) const;
@@ -196,7 +196,7 @@ protected:
 
 protected:
 	void AddVisibleItemToIndex(HTREEITEM hti) const;
-	void AddItemData(HTREEITEM hti, CDWordArray& aItemData, BOOL bIncParents, BOOL bInCollapsedChildren) const;
+	void AddItemData(HTREEITEM hti, CDWordArray& aItemData, BOOL bIncParents, BOOL bIncCollapsedChildren) const;
 	int GetPageCount() const;
 
 	TCH_WHERE GetMoveTarget(HTREEITEM htiDestParent, HTREEITEM htiDestPrevSibling, HTREEITEM& htiTarget) const;
