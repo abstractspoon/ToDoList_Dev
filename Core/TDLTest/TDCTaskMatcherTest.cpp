8c7514cbaead9407cfd2aefaa044b4516a3c0552
abstractspoon <abstractspoon@users.noreply.github.com>
2024-09-25 19:51:23 +1000

    Refactor tests to explicitly use rule objects

--- Core/TDLTest/TDCTaskMatcherTest.cpp
+++ Core/TDLTest/TDCTaskMatcherTest.cpp
@@ -104,51 +104,99 @@ void CTDCTaskMatcherTest::TestSimpleTextMatch(const CToDoCtrlData& data, TDC_ATT
 	CTDCTaskMatcher match(data, reminders, contentMgr);
 	CDWordArray aTaskIDs; // get overwritten by each test
 
+	SEARCHPARAM rule(nAttibID);
+
 	// Equals
 	{
-		ExpectEQ(1, match.FindTasks(nAttibID, FOP_EQUALS, (sPrefix + _T("2")), FALSE, aTaskIDs));
-		ExpectTrue(ContainsOnlyOneTaskID(aTaskIDs, 2));
+		rule.SetOperator(FOP_EQUALS);
 
-		ExpectEQ(0, match.FindTasks(nAttibID, FOP_EQUALS, _T("Equals"), FALSE, aTaskIDs));
-		ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
+		{
+			rule.SetValue(sPrefix + _T("2"));
+
+			ExpectEQ(1, match.FindTasks(rule, FALSE, aTaskIDs));
+			ExpectTrue(ContainsOnlyOneTaskID(aTaskIDs, 2));
+		}
+
+		{
+			rule.SetValue(_T("Equals"));
+
+			ExpectEQ(0, match.FindTasks(rule, FALSE, aTaskIDs));
+			ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
+		}
 	}
 
 	// Not equals
 	{
-		ExpectEQ(5, match.FindTasks(nAttibID, FOP_NOT_EQUALS, (sPrefix + _T("5")), FALSE, aTaskIDs));
-		ExpectTrue(ContainsAllButOneTaskIDs(aTaskIDs, 5));
+		rule.SetOperator(FOP_NOT_EQUALS);
 
-		ExpectEQ(6, match.FindTasks(nAttibID, FOP_NOT_EQUALS, _T("NotEquals"), FALSE, aTaskIDs));
-		ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
+		{
+			rule.SetValue(sPrefix + _T("5"));
+
+			ExpectEQ(5, match.FindTasks(rule, FALSE, aTaskIDs));
+			ExpectTrue(ContainsAllButOneTaskIDs(aTaskIDs, 5));
+		}
+
+		{
+			rule.SetValue(_T("NotEquals"));
+
+			ExpectEQ(6, match.FindTasks(rule, FALSE, aTaskIDs));
+			ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
+		}
 	}
 
 	// Includes
 	{
-		ExpectEQ(6, match.FindTasks(nAttibID, FOP_INCLUDES, sPrefix, FALSE, aTaskIDs));
-		ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
+		rule.SetOperator(FOP_INCLUDES);
 
-		ExpectEQ(0, match.FindTasks(nAttibID, FOP_INCLUDES, _T("Includes"), FALSE, aTaskIDs));
-		ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
+		{
+			rule.SetValue(sPrefix);
+
+			ExpectEQ(6, match.FindTasks(rule, FALSE, aTaskIDs));
+			ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
+		}
+
+		{
+			rule.SetValue(_T("Includes"));
+
+			ExpectEQ(0, match.FindTasks(rule, FALSE, aTaskIDs));
+			ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
+		}
 	}
 
 	// Not includes
 	{
-		ExpectEQ(0, match.FindTasks(nAttibID, FOP_NOT_INCLUDES, sPrefix, FALSE, aTaskIDs));
-		ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
+		rule.SetOperator(FOP_NOT_INCLUDES);
 
-		ExpectEQ(6, match.FindTasks(nAttibID, FOP_NOT_INCLUDES, _T("NotIncludes"), FALSE, aTaskIDs));
-		ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
+		{
+			rule.SetValue(sPrefix);
+
+			ExpectEQ(0, match.FindTasks(rule, FALSE, aTaskIDs));
+			ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
+		}
+
+		{
+			rule.SetValue(_T("NotIncludes"));
+
+			ExpectEQ(6, match.FindTasks(rule, FALSE, aTaskIDs));
+			ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
+		}
 	}
 
 	// Set
 	{
-		ExpectEQ(6, match.FindTasks(nAttibID, FOP_SET, _T(""), FALSE, aTaskIDs));
+		rule.SetOperator(FOP_SET);
+		// value ignored
+
+		ExpectEQ(6, match.FindTasks(rule, FALSE, aTaskIDs));
 		ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
 	}
 
 	// Not set
 	{
-		ExpectEQ(0, match.FindTasks(nAttibID, FOP_NOT_SET, _T(""), FALSE, aTaskIDs));
+		rule.SetOperator(FOP_NOT_SET);
+		// value ignored
+
+		ExpectEQ(0, match.FindTasks(rule, FALSE, aTaskIDs));
 		ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
 	}
 }
@@ -158,47 +206,50 @@ void CTDCTaskMatcherTest::TestSimpleTextArrayMatch(const CToDoCtrlData& data, TD
 	CTDCTaskMatcher match(data, reminders, contentMgr);
 	CDWordArray aTaskIDs; // get overwritten by each test
 
-	CStringArray aValues;
-	CString sCompare;
+	SEARCHPARAM rule(nAttibID);
 
 	// Equals
 	{
-		{
-			sCompare.Format(_T("%s2.0, %s2.1, %s2.2"), sPrefix, sPrefix, sPrefix);
+		rule.SetOperator(FOP_EQUALS);
 
-			ExpectEQ(1, match.FindTasks(nAttibID, FOP_EQUALS, sCompare, FALSE, aTaskIDs));
+		{
+			rule.SetValue(Misc::Format(_T("%s2.0, %s2.1, %s2.2"), sPrefix, sPrefix, sPrefix));
+
+			ExpectEQ(1, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsOnlyOneTaskID(aTaskIDs, 2));
 		}
 
 		// Not order-sensitive
 		{
-			sCompare.Format(_T("%s3.1, %s3.2, %s3.0"), sPrefix, sPrefix, sPrefix);
+			rule.SetValue(Misc::Format(_T("%s3.1, %s3.2, %s3.0"), sPrefix, sPrefix, sPrefix));
 
-			ExpectEQ(1, match.FindTasks(nAttibID, FOP_EQUALS, sCompare, FALSE, aTaskIDs));
+			ExpectEQ(1, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsOnlyOneTaskID(aTaskIDs, 3));
 		}
 	}
 
 	// Not equals
 	{
-		{
-			sCompare.Format(_T("%s4.0"), sPrefix);
+		rule.SetOperator(FOP_NOT_EQUALS);
 
-			ExpectEQ(6, match.FindTasks(nAttibID, FOP_NOT_EQUALS, sCompare, FALSE, aTaskIDs));
+		{
+			rule.SetValue(Misc::Format(_T("%s4.0"), sPrefix));
+
+			ExpectEQ(6, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
 		}
 
 		{
-			sCompare.Format(_T("%s2.0, %s2.1, %s2.2"), sPrefix, sPrefix, sPrefix);
+			rule.SetValue(Misc::Format(_T("%s2.0, %s2.1, %s2.2"), sPrefix, sPrefix, sPrefix));
 
-			ExpectEQ(5, match.FindTasks(nAttibID, FOP_NOT_EQUALS, sCompare, FALSE, aTaskIDs));
+			ExpectEQ(5, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsAllButOneTaskIDs(aTaskIDs, 2));
 		}
 
 		{
-			sCompare = _T("Cat3.0, Cat3.1, Cat3.2");
+			rule.SetValue(_T("Cat3.0, Cat3.1, Cat3.2"));
 
-			int nNumMatches = match.FindTasks(nAttibID, FOP_NOT_EQUALS, sCompare, FALSE, aTaskIDs);
+			int nNumMatches = match.FindTasks(rule, FALSE, aTaskIDs);
 
 			if (nNumMatches == 6)
 			{
@@ -216,69 +267,99 @@ void CTDCTaskMatcherTest::TestSimpleTextArrayMatch(const CToDoCtrlData& data, TD
 
 	// Includes
 	{
+		rule.SetOperator(FOP_INCLUDES);
+
 		{
-			ExpectEQ(6, match.FindTasks(nAttibID, FOP_INCLUDES, sPrefix, FALSE, aTaskIDs));
+			rule.SetValue(sPrefix);
+
+			ExpectEQ(6, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
 		}
 
 		{
-			ExpectEQ(6, match.FindTasks(nAttibID, FOP_INCLUDES, _T(".2"), FALSE, aTaskIDs));
+			rule.SetValue(_T(".2"));
+
+			ExpectEQ(6, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
 		}
 
 		{
-			ExpectEQ(1, match.FindTasks(nAttibID, FOP_INCLUDES, (sPrefix + _T("3")), FALSE, aTaskIDs));
+			rule.SetValue(sPrefix + _T("3"));
+
+			ExpectEQ(1, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsOnlyOneTaskID(aTaskIDs, 3));
 		}
 
 		{
-			ExpectEQ(1, match.FindTasks(nAttibID, FOP_INCLUDES, _T("5"), FALSE, aTaskIDs));
+			rule.SetValue(_T("5"));
+
+			ExpectEQ(1, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsOnlyOneTaskID(aTaskIDs, 5));
 		}
 
 		{
-			ExpectEQ(0, match.FindTasks(nAttibID, FOP_INCLUDES, _T("Includes"), FALSE, aTaskIDs));
+			rule.SetValue(_T("Includes"));
+
+			ExpectEQ(0, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
 		}
 	}
 
 	// Not includes
 	{
+		rule.SetOperator(FOP_NOT_INCLUDES);
+
 		{
-			ExpectEQ(0, match.FindTasks(nAttibID, FOP_NOT_INCLUDES, sPrefix, FALSE, aTaskIDs));
+			rule.SetValue(sPrefix);
+
+			ExpectEQ(0, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
 		}
 
 		{
-			ExpectEQ(5, match.FindTasks(nAttibID, FOP_NOT_INCLUDES, (sPrefix + _T("1")), FALSE, aTaskIDs));
+			rule.SetValue(sPrefix + _T("1"));
+
+			ExpectEQ(5, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsAllButOneTaskIDs(aTaskIDs, 1));
 		}
 
 		{
-			ExpectEQ(0, match.FindTasks(nAttibID, FOP_NOT_INCLUDES, _T("0"), FALSE, aTaskIDs));
+			rule.SetValue(_T("0"));
+
+			ExpectEQ(0, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
 		}
 
 		{
-			ExpectEQ(6, match.FindTasks(nAttibID, FOP_NOT_INCLUDES, _T(".4"), FALSE, aTaskIDs));
+			rule.SetValue(_T(".4"));
+
+			ExpectEQ(6, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
 		}
 
 		{
-			ExpectEQ(6, match.FindTasks(nAttibID, FOP_NOT_INCLUDES, _T("NotIncludes"), FALSE, aTaskIDs));
+			rule.SetValue(_T("NotIncludes"));
+
+			ExpectEQ(6, match.FindTasks(rule, FALSE, aTaskIDs));
 			ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
 		}
 	}
 
 	// Set
 	{
-		ExpectEQ(6, match.FindTasks(nAttibID, FOP_SET, _T(""), FALSE, aTaskIDs));
+		rule.SetOperator(FOP_SET);
+		// value ignored
+
+		ExpectEQ(6, match.FindTasks(rule, FALSE, aTaskIDs));
 		ExpectTrue(ContainsAllTaskIDs(aTaskIDs));
 	}
 
 	// Not set
 	{
-		ExpectEQ(0, match.FindTasks(nAttibID, FOP_NOT_SET, _T(""), FALSE, aTaskIDs));
+		rule.SetOperator(FOP_NOT_SET);
+		// value ignored
+
+		ExpectEQ(0, match.FindTasks(rule, FALSE, aTaskIDs));
 		ExpectTrue(ContainsNoTaskIDs(aTaskIDs));
 	}
 }
