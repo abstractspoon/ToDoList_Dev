dc9e0fcce06e20913f09d604bc21d6ddda41f251
abstractspoon <abstractspoon@users.noreply.github.com>
2025-06-10 18:25:01 +1000

    Remove whitespace

--- Core/Shared/GraphicsMisc.cpp
+++ Core/Shared/GraphicsMisc.cpp
@@ -184,18 +184,19 @@ void GraphicsMisc::DrawGradient(CDC* pDC, LPCRECT pRect, COLORREF crFrom, COLORR
 	TRIVERTEX vert[2];
 	GRADIENT_RECT gRect;
 
-	vert[0] .x      = pRect->left;
-	vert[0] .y      = pRect->top;
-	vert[0] .Red    = MAKEWORD(0, GetRValue(crFrom));
-	vert[0] .Green  = MAKEWORD(0, GetGValue(crFrom));
-	vert[0] .Blue   = MAKEWORD(0, GetBValue(crFrom));
-	vert[0] .Alpha  = 0x0000;
-	vert[1] .x      = pRect->right;
-	vert[1] .y      = pRect->bottom; 
-	vert[1] .Red    = MAKEWORD(0, GetRValue(crTo));
-	vert[1] .Green  = MAKEWORD(0, GetGValue(crTo));
-	vert[1] .Blue   = MAKEWORD(0, GetBValue(crTo));
-	vert[1] .Alpha  = 0x0000;
+	vert[0].x      = pRect->left;
+	vert[0].y      = pRect->top;
+	vert[0].Red    = MAKEWORD(0, GetRValue(crFrom));
+	vert[0].Green  = MAKEWORD(0, GetGValue(crFrom));
+	vert[0].Blue   = MAKEWORD(0, GetBValue(crFrom));
+	vert[0].Alpha  = 0x0000;
+	vert[1].x      = pRect->right;
+	vert[1].y      = pRect->bottom; 
+	vert[1].Red    = MAKEWORD(0, GetRValue(crTo));
+	vert[1].Green  = MAKEWORD(0, GetGValue(crTo));
+	vert[1].Blue   = MAKEWORD(0, GetBValue(crTo));
+	vert[1].Alpha  = 0x0000;
+
 	gRect.UpperLeft  = 0;
 	gRect.LowerRight = 1;
 
