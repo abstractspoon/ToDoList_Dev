4ad3a5be37b87deb0e9c96fb1a5a07a13062fc72
abstractspoon <abstractspoon@users.noreply.github.com>
2025-07-17 20:27:51 +1000

    Merge build script fix from ToDoList_9.1

--- BuildReleaseZip.bat
+++ BuildReleaseZip.bat
@@ -221,7 +221,18 @@ SET FILELIST=%FILELIST%;XmlDiffView.exe
 SET FILELIST=%FILELIST%;ZstdNet.dll
 
 FOR %%f IN (%FILELIST%) DO ( 
+ECHO Adding '%%f' to zip >> %OUTPUT_FILE%
 %PATH7ZIP% a %OUTZIP% %OUTDIR%\%%f >> %OUTPUT_FILE%
+
+REM - Check for locking errors
+IF %errorlevel% NEQ 0 (
+ECHO [41m Zipping Binaries FAILED[0m
+ECHO:
+REM Open the log
+%OUTPUT_FILE%
+PAUSE
+EXIT
+)
 )
 
 REM REquired MS components for C# plugins
