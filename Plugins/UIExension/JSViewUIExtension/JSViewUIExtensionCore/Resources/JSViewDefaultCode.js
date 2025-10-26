d80c23f9a9ab5c47032b84005ed0b76ee4faa691
abstractspoon <abstractspoon@users.noreply.github.com>
2025-10-26 18:39:10 +1100

    Add 2x2 table to html, each containing a placeholder graph

--- Plugins/UIExension/JSViewUIExtension/JSViewUIExtensionCore/Resources/JSViewDefaultCode.js
+++ Plugins/UIExension/JSViewUIExtension/JSViewUIExtensionCore/Resources/JSViewDefaultCode.js
@@ -1,27 +1,35 @@
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawCharts);
 
-function drawChart() 
+function drawCharts()
 {
-  var data = new google.visualization.DataTable();
+    drawChart('curve_chart11');
+    drawChart('curve_chart12');
+    drawChart('curve_chart21');
+    drawChart('curve_chart22');
+}
+
+
+function drawChart(chartName) 
+{
+    var data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Year');
+    data.addColumn('number', 'Sales');
+    data.addColumn('number', 'Expenses');
   
-  data.addColumn('string', 'Year');
-  data.addColumn('number', 'Sales');
-  data.addColumn('number', 'Expenses');
-  
-  for (let i = 0; i < 5; i++) 
-  {  
-	  data.addRow([tasks[i].Title, tasks[i].Priority, tasks[i].Risk]);
-  }
+    for (let i = 0; i < 5; i++) 
+    {  
+        data.addRow([tasks[i].Title, tasks[i].Priority, tasks[i].Risk]);
+    }
 
-  var options = 
-  {
-    title: 'Company Performance',
-    curveType: 'function',
-    legend: { position: 'bottom' }
-  };
+    var options = 
+    {
+        title: 'Company Performance',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
 
-  var chart = new google.visualization.BarChart(document.getElementById('curve_chart'));
-
-  chart.draw(data, options);
+    var chart = new google.visualization.BarChart(document.getElementById(chartName));
+    chart.draw(data, options);
 }
