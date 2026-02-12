c2eac15e676855fff28781653e020c67bcc08877
.dan.g. <abstractspoon@users.noreply.github.com>
2026-02-12 15:11:56 +1100

    Fix TreeList row misalignment on Ubuntu

--- Core/Shared/TreeListSyncer.cpp
+++ Core/Shared/TreeListSyncer.cpp
@@ -27,7 +27,7 @@ const int MAX_SPLITBAR_WIDTH	= GraphicsMisc::ScaleByDPIFactor(6);
 const int MIN_SPLITBAR_WIDTH	= 1; // don't scale
 const int MIN_SPLIT_WIDTH		= (GetSystemMetrics(SM_CXVSCROLL) * 2);
 const int INITIAL_SPLIT_POS		= GraphicsMisc::ScaleByDPIFactor(300);
-const int LINUX_VOFFSET_FUDGE	= 2;
+const int LINUX_VOFFSET_FUDGE	= 1;
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
