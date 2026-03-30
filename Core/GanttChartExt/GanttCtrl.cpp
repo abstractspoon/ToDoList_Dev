121c2918791750d5c80b6e0312d9ade5b56ae5a3
abstractspoon <abstractspoon@users.noreply.github.com>
2026-03-30 19:43:28 +1100

    Remove obsolete code

--- Core/GanttChartExt/GanttCtrl.cpp
+++ Core/GanttChartExt/GanttCtrl.cpp
@@ -162,16 +162,16 @@ DWORD CGanttCtrl::GetSelectedTaskID() const
 	return GetSelectedItemData();
 }
 
-BOOL CGanttCtrl::GetSelectedTaskDependencies(CDWordArray& aDepends) const
-{
-	DWORD dwTaskID = GetSelectedTaskID();
-	const GANTTITEM* pGI = NULL;
-	
-	GET_GI_RET(dwTaskID, pGI, FALSE);
-	
-	aDepends.Copy(pGI->aDependIDs);
-	return TRUE;
-}
+// BOOL CGanttCtrl::GetSelectedTaskDependencies(CDWordArray& aDepends) const
+// {
+// 	DWORD dwTaskID = GetSelectedTaskID();
+// 	const GANTTITEM* pGI = NULL;
+// 	
+// 	GET_GI_RET(dwTaskID, pGI, FALSE);
+// 	
+// 	aDepends.Copy(pGI->aDependIDs);
+// 	return TRUE;
+// }
 
 BOOL CGanttCtrl::AddSelectedTaskDependency(DWORD dwDependID)
 {
