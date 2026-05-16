0916ff4127d635b57cdc798d2ed0d140174212b9
abstractspoon <abstractspoon@users.noreply.github.com>
2026-05-16 19:52:27 +1000

    Merge due task notification fix from ToDoList_9.2

--- Core/ToDoList/ToDoListWnd.cpp
+++ Core/ToDoList/ToDoListWnd.cpp
@@ -5005,7 +5005,7 @@ TDCEXPORTTASKLIST* CToDoListWnd::PrepareNewDueTaskNotification(int nTDC, int nDu
 	}
 	
 	// set an appropriate title
-	pExport->tasks.SetReportDetails(CEnString(nIDDueBy));
+	pExport->tasks.SetReportDetails(CEnString(nIDDueBy), CDateHelper::NullDate());
 
 	if (bTransform)
 		pExport->sStylesheet = sStylesheet;
