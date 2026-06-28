534d8272c48fffacac969644699e52258970c092
abstractspoon <abstractspoon@users.noreply.github.com>
2026-06-28 17:41:18 +1000

    Prevent unnecessary selection updates

--- Plugins/UIExtension/LoggedTimeUIExtension/LoggedTimeUIExtensionBridge/LoggedTimeUIExtensionBridge.h
+++ Plugins/UIExtension/LoggedTimeUIExtension/LoggedTimeUIExtensionBridge/LoggedTimeUIExtensionBridge.h
@@ -31,7 +31,7 @@ public:
    void SavePreferences(IPreferences* pPrefs, LPCWSTR szKey) const;
    void LoadPreferences(const IPreferences* pPrefs, LPCWSTR szKey);
 
-   bool SupportsTaskSelection() const { return true; }
+   bool SupportsTaskSelection() const { return false; }
 
 protected:
 	HICON m_hIcon;
