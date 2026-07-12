f149878fc374297118c41e0db3e542d1c91b4ccc
abstractspoon <abstractspoon@users.noreply.github.com>
2026-07-12 20:53:16 +1000

    Merge 'Kanban' indentation fix from ToDoList_9.2

--- Core/KanbanBoard/KanbanColumnCtrl.cpp
+++ Core/KanbanBoard/KanbanColumnCtrl.cpp
@@ -83,7 +83,6 @@ const int MIN_LABEL_EDIT_WIDTH	= GraphicsMisc::ScaleByDPIFactor(200);
 const int BAR_WIDTH				= GraphicsMisc::ScaleByDPIFactor(6);
 const int LV_PADDING			= GraphicsMisc::ScaleByDPIFactor(3);
 const int CHECKBOX_PADDING		= GraphicsMisc::ScaleByDPIFactor(1);
-const int ATTRIB_INDENT			= GraphicsMisc::ScaleByDPIFactor(6);
 const int TIP_PADDING			= GraphicsMisc::ScaleByDPIFactor(4);
 const int DEF_IMAGE_SIZE		= GraphicsMisc::ScaleByDPIFactor(16);
 const int LEVEL_INDENT			= GraphicsMisc::ScaleByDPIFactor(16);
@@ -618,13 +617,18 @@ int CKanbanColumnCtrl::CalcAvailableAttributeWidth(int nColWidth) const
 		nColWidth -= GetSystemMetrics(SM_CXVSCROLL);
 	}
 
+	if (HasOption(KBCF_SHOWCOMPLETIONCHECKBOXES))
+	{
+		nColWidth -= CEnImageList::GetImageSize(m_ilCheckboxes);
+		nColWidth -= (CHECKBOX_PADDING * 2);
+	}
+
 	int nAvailWidth = (nColWidth - (2 * LV_PADDING));
 
 	if (HasOption(KBCF_SHOWTASKCOLORASBAR))
 		nAvailWidth -= BAR_WIDTH;
 
 	nAvailWidth -= (TEXT_BORDER.left + TEXT_BORDER.right);
-	nAvailWidth -= ATTRIB_INDENT;
 
 	return nAvailWidth;
 }
@@ -1155,6 +1159,8 @@ void CKanbanColumnCtrl::DrawItemBar(CDC* pDC, const KANBANITEM* pKI, CRect& rIte
 
 		rItem.left = rBar.right;
 	}
+
+	rItem.left += TEXT_BORDER.left;
 }
 
 void CKanbanColumnCtrl::DrawItemCheckbox(CDC* pDC, const KANBANITEM* pKI, CRect& rItem)
