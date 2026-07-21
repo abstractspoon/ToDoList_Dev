a5170483dea449d815d8996f874ef31eca197be9
abstractspoon <abstractspoon@users.noreply.github.com>
2026-07-21 12:31:12 +1000

    Fix resource copy script

--- CopyResToBuildFolders.bat
+++ CopyResToBuildFolders.bat
@@ -10,25 +10,36 @@ ECHO RESREPO=%RESREPO%
 set TESTREPO=%REPO%\..\ToDoList_Test
 ECHO TESTREPO=%TESTREPO%
 
+set EXCLUDEFILE=%REPO%\CopyResToBuildFolders_Exclude.txt
+
 REM - Copy to Core
-xcopy %RESREPO%\*.* %REPO%\Core\ToDoList\Unicode_Debug\Resources\   /E /EXCLUDE:%REPO%\CopyResToBuildFolders_Exclude.txt /Y 
-xcopy %RESREPO%\*.* %REPO%\Core\ToDoList\Unicode_Release\Resources\   /E /EXCLUDE:%REPO%\CopyResToBuildFolders_Exclude.txt /Y 
+xcopy %RESREPO%\*.* %REPO%\Core\ToDoList\Unicode_Debug\Resources\  /E /EXCLUDE:%EXCLUDEFILE% /Y 
+xcopy %RESREPO%\*.* %REPO%\Core\ToDoList\Unicode_Release\Resources\  /E /EXCLUDE:%EXCLUDEFILE% /Y 
 
-xcopy %TESTREPO%\Tasklists\*.* /E %REPO%\Core\ToDoList\Unicode_Debug\Resources\Examples\  /Y   
-xcopy %TESTREPO%\Tasklists\*.* /E %REPO%\Core\ToDoList\Unicode_Release\Resources\Examples\  /Y   
+xcopy %RESREPO%\*.jpg %REPO%\Core\ToDoList\Unicode_Debug\Resources\Icons\  /s /Y 
+xcopy %RESREPO%\*.jpg %REPO%\Core\ToDoList\Unicode_Release\Resources\Icon\  /s /Y 
 
-xcopy "%TESTREPO%\Sample Languages\*.*" /E %REPO%\Core\ToDoList\Unicode_Debug\Resources\Examples\  /Y   
-xcopy "%TESTREPO%\Sample Languages\*.*" /E %REPO%\Core\ToDoList\Unicode_Release\Resources\Examples\  /Y   
+xcopy %TESTREPO%\Tasklists\*.* %REPO%\Core\ToDoList\Unicode_Debug\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y 
+xcopy %TESTREPO%\Tasklists\*.* %REPO%\Core\ToDoList\Unicode_Release\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y  
+
+xcopy %TESTREPO%\Checklists\*.* %REPO%\Core\ToDoList\Unicode_Debug\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y  
+xcopy %TESTREPO%\Checklists\*.* %REPO%\Core\ToDoList\Unicode_Release\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y   
+
+xcopy "%TESTREPO%\Sample Languages\*.*" %REPO%\Core\ToDoList\Unicode_Debug\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y   
+xcopy "%TESTREPO%\Sample Languages\*.*" %REPO%\Core\ToDoList\Unicode_Release\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y   
 
 REM - Copy Resources to Plugins
-xcopy %RESREPO%\*.* %REPO%\Plugins\Debug\Resources\   /E /EXCLUDE:%REPO%\CopyResToBuildFolders_Exclude.txt /Y 
-xcopy %RESREPO%\*.* %REPO%\Plugins\Release\Resources\ /E /EXCLUDE:%REPO%\CopyResToBuildFolders_Exclude.txt /Y 
+xcopy %RESREPO%\*.* %REPO%\Plugins\Debug\Resources\  /E /EXCLUDE:%EXCLUDEFILE% /Y 
+xcopy %RESREPO%\*.* %REPO%\Plugins\Release\Resources\  /E /EXCLUDE:%EXCLUDEFILE% /Y 
 
-xcopy %TESTREPO%\Tasklists\*.* /E %REPO%\Plugins\Debug\Resources\Examples\  /Y   
-xcopy %TESTREPO%\Tasklists\*.* /E %REPO%\Plugins\Release\Resources\Examples\  /Y   
+xcopy %TESTREPO%\Tasklists\*.* %REPO%\Plugins\Debug\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y  
+xcopy %TESTREPO%\Tasklists\*.* %REPO%\Plugins\Release\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y   
 
-xcopy "%TESTREPO%\Sample Languages\*.*" /E %REPO%\Plugins\Debug\Resources\Examples\  /Y   
-xcopy "%TESTREPO%\Sample Languages\*.*" /E %REPO%\Plugins\Release\Resources\Examples\  /Y   
+xcopy %TESTREPO%\Checklists\*.* %REPO%\Plugins\Debug\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y   
+xcopy %TESTREPO%\Checklists\*.* %REPO%\Plugins\Release\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y   
+
+xcopy "%TESTREPO%\Sample Languages\*.*" %REPO%\Plugins\Debug\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y 
+xcopy "%TESTREPO%\Sample Languages\*.*" %REPO%\Plugins\Release\Resources\Examples\  /E /EXCLUDE:%EXCLUDEFILE% /Y  
 
 popd
 pause
