32d56dd31cdf9f89a4aa014cd3747fb0a83b1442
abstractspoon <abstractspoon@users.noreply.github.com>
2026-08-18 19:05:13 +1000

    Remove MFC boilerplate comments

--- Core/Shared/InputListCtrl.h
+++ Core/Shared/InputListCtrl.h
@@ -37,7 +37,6 @@ class CInputListCtrl : public CEnListCtrl
 {
 	DECLARE_DYNAMIC(CInputListCtrl)
 
-// Construction
 public:
 	CInputListCtrl();
 	virtual ~CInputListCtrl();
@@ -113,14 +112,12 @@ protected:
 	virtual BOOL PreTranslateMessage(MSG* pMsg);
 
 protected:
-	//{{AFX_MSG(CInputListCtrl)
 	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
 	afx_msg void OnKillFocus(CWnd* pNewWnd);
 	afx_msg void OnSetFocus(CWnd* pOldWnd);
 	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
 	afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
-	//}}AFX_MSG
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnLButtonDblClk(UINT nFlags, CPoint point);
 	afx_msg void OnSize(UINT nType, int cx, int cy);
@@ -201,8 +198,5 @@ private:
 };
 
 /////////////////////////////////////////////////////////////////////////////
-
-//{{AFX_INSERT_LOCATION}}
-// Microsoft Developer Studio will insert additional declarations immediately before the previous line.
 
 #endif // !defined(AFX_INPUTLISTCTRL_H__2E5810B0_D7DF_11D1_AB19_0000E8425C3E__INCLUDED_)
