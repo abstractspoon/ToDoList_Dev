9fc354201ed1f1b75521c23b6601b3e24f6f7eb7
abstractspoon <abstractspoon@users.noreply.github.com>
2026-09-14 18:14:42 +1000

    Create basic projects framework copied from SampleImpExp

---
+++ Plugins/ImportExport/TodoTxtImpExp/TodoTxtImpExpBridge/TodoTxtImpExpBridge.cpp
@@ -0,0 +1,119 @@
+// ExporterBridge.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "TodoTxtImpExpBridge.h"
+#include "resource.h"
+
+#include <unknwn.h>
+#include <tchar.h>
+#include <msclr\auto_gcroot.h>
+
+#include <Interfaces\ITasklist.h>
+#include <Interfaces\ITransText.h>
+#include <Interfaces\IPreferences.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#using <PluginHelpers.dll> as_friend
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+using namespace TodoTxtImpExp;
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Runtime::InteropServices;
+
+using namespace Abstractspoon::Tdl::PluginHelpers;
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// This is the constructor of a class that has been exported.
+// see ExporterBridge.h for the class definition
+CTodoTxtImpExpBridge::CTodoTxtImpExpBridge()
+	:
+	m_hIcon(NULL),
+	m_pTT(nullptr)
+{
+	m_hIcon = Win32::LoadHIcon(L"TodoTxtImpExpBridge.dll", IDI_TODOTXT, 16, true);
+}
+
+void CTodoTxtImpExpBridge::Release()
+{
+	delete this;
+}
+
+void CTodoTxtImpExpBridge::SetLocalizer(ITransText* pTT)
+{
+	if (m_pTT == nullptr)
+		m_pTT = pTT;
+}
+
+HICON CTodoTxtImpExpBridge::GetIcon() const
+{
+	return m_hIcon;
+}
+
+LPCWSTR CTodoTxtImpExpBridge::GetMenuText() const
+{
+	return L"todo.txt";
+}
+
+LPCWSTR CTodoTxtImpExpBridge::GetFileFilter() const
+{
+	return L"txt";
+}
+
+LPCWSTR CTodoTxtImpExpBridge::GetFileExtension() const
+{
+	return L"txt";
+}
+
+LPCWSTR CTodoTxtImpExpBridge::GetTypeID() const
+{
+	return L"E18EBEBB-4F06-476B-B3C9-069A6B25D521";
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// IExportTasklist
+IIMPORTEXPORT_RESULT CTodoTxtImpExpBridge::Export(const ITaskList* pSrcTaskFile, LPCWSTR szDestFilePath, DWORD dwFlags, IPreferences* pPrefs, LPCWSTR szKey)
+{
+	auto exporter = gcnew TodoTxtExporter(gcnew Translator(m_pTT));
+
+	if (exporter->Export(gcnew TaskList(pSrcTaskFile),
+						 gcnew String(szDestFilePath),
+						 ((dwFlags & IIEF_SILENT) != 0),
+						 gcnew Preferences(pPrefs),
+						 gcnew String(szKey)))
+	{
+		return IIER_SUCCESS;
+	}
+
+	// else
+	return IIER_OTHER;
+}
+
+IIMPORTEXPORT_RESULT CTodoTxtImpExpBridge::Export(const IMultiTaskList* pSrcTaskFile, LPCWSTR szDestFilePath, DWORD dwFlags, IPreferences* pPrefs, LPCWSTR szKey)
+{
+	// TODO
+	return IIER_OTHER;
+}
+
+// IImportTasklist
+IIMPORTEXPORT_RESULT CTodoTxtImpExpBridge::Import(LPCWSTR szSrcFilePath, ITaskList* pDestTaskFile, DWORD dwFlags, IPreferences* pPrefs, LPCWSTR szKey)
+{
+	auto importer = gcnew TodoTxtImporter(gcnew Translator(m_pTT));
+	
+	if (importer->Import(gcnew String(szSrcFilePath),
+						 gcnew TaskList(pDestTaskFile),
+						 ((dwFlags & IIEF_SILENT) != 0),
+						 gcnew Preferences(pPrefs),
+						 gcnew String(szKey)))
+	{
+		return IIER_SUCCESS;
+	}
+
+	// else
+	return IIER_OTHER;
+}
---
+++ Plugins/ImportExport/TodoTxtImpExp/TodoTxtImpExpCore/TodoTxtImporter.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Abstractspoon.Tdl.PluginHelpers;
+
+namespace TodoTxtImpExp
+{
+    [System.ComponentModel.DesignerCategory("")]
+
+    public class TodoTxtImporter
+    {
+		private Translator m_Trans;
+
+        // ----------------------------------------------------------
+
+		public TodoTxtImporter(Translator trans)
+        {
+            m_Trans = trans;
+        }
+
+		public bool Import(string srcFilePath, TaskList destTaskFile, uint flags, Preferences prefs, string prefKey)
+        {
+			// Possibly display a dialog to get input on how to 
+			// map ToDoList task attributes to the output format
+			// TODO
+
+            // Process the tasks
+			var srcTasks = new ToDoLib.TaskList(srcFilePath);
+
+			foreach (var srcTask in srcTasks.Tasks)
+			{
+
+
+			}
+
+            return true;
+        }
+
+//         protected bool ExportTask(Task task /*, probably with some additional parameters*/)
+//         {
+//             // Process task's own attributes
+//             // TODO
+// 
+//             // Export task's children
+//             Task subtask = task.GetFirstSubtask();
+// 
+//             while (subtask.IsValid())
+//             {
+//                 if (!ExportTask(subtask /*, probably with some additional parameters*/ ))
+//                 {
+//                     // Decide whether to stop or not
+//                     // TODO
+//                 }
+// 
+//                 subtask = subtask.GetNextTask();
+//             }
+// 
+//             return true;
+//         }
+
+        // ----------------------------------------------------------
+    }
+}
