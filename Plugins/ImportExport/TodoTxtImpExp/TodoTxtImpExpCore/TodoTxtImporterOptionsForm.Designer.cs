a023f21e804c91d30ba9b21da401b8a29e34d07d
abstractspoon <abstractspoon@users.noreply.github.com>
2026-09-17 20:23:33 +1000

    Fix importer options dialog background colour

--- Plugins/ImportExport/TodoTxtImpExp/TodoTxtImpExpCore/TodoTxtImporterOptionsForm.Designer.cs
+++ Plugins/ImportExport/TodoTxtImpExp/TodoTxtImpExpCore/TodoTxtImporterOptionsForm.Designer.cs
@@ -38,14 +38,16 @@
 			this.m_Cancel = new System.Windows.Forms.Button();
 			this.m_ContextGroup = new System.Windows.Forms.GroupBox();
 			this.m_ProjectGroup = new System.Windows.Forms.GroupBox();
+			this.panel1 = new System.Windows.Forms.Panel();
 			this.m_ContextGroup.SuspendLayout();
 			this.m_ProjectGroup.SuspendLayout();
+			this.panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(8, 9);
+			this.label1.Location = new System.Drawing.Point(12, 14);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(0, 13);
 			this.label1.TabIndex = 0;
@@ -54,6 +56,7 @@
 			// 
 			this.m_ContextAsCategoryBtn.AutoSize = true;
 			this.m_ContextAsCategoryBtn.Checked = true;
+			this.m_ContextAsCategoryBtn.ForeColor = System.Drawing.SystemColors.WindowText;
 			this.m_ContextAsCategoryBtn.Location = new System.Drawing.Point(15, 19);
 			this.m_ContextAsCategoryBtn.Name = "m_ContextAsCategoryBtn";
 			this.m_ContextAsCategoryBtn.Size = new System.Drawing.Size(75, 17);
@@ -65,6 +68,7 @@
 			// m_ContextAsTagBtn
 			// 
 			this.m_ContextAsTagBtn.AutoSize = true;
+			this.m_ContextAsTagBtn.ForeColor = System.Drawing.SystemColors.WindowText;
 			this.m_ContextAsTagBtn.Location = new System.Drawing.Point(15, 42);
 			this.m_ContextAsTagBtn.Name = "m_ContextAsTagBtn";
 			this.m_ContextAsTagBtn.Size = new System.Drawing.Size(49, 17);
@@ -75,6 +79,7 @@
 			// m_ProjectAsCategoryBtn
 			// 
 			this.m_ProjectAsCategoryBtn.AutoSize = true;
+			this.m_ProjectAsCategoryBtn.ForeColor = System.Drawing.SystemColors.WindowText;
 			this.m_ProjectAsCategoryBtn.Location = new System.Drawing.Point(14, 19);
 			this.m_ProjectAsCategoryBtn.Name = "m_ProjectAsCategoryBtn";
 			this.m_ProjectAsCategoryBtn.Size = new System.Drawing.Size(75, 17);
@@ -85,6 +90,7 @@
 			// m_ProjectAsTagBtn
 			// 
 			this.m_ProjectAsTagBtn.AutoSize = true;
+			this.m_ProjectAsTagBtn.ForeColor = System.Drawing.SystemColors.WindowText;
 			this.m_ProjectAsTagBtn.Location = new System.Drawing.Point(14, 42);
 			this.m_ProjectAsTagBtn.Name = "m_ProjectAsTagBtn";
 			this.m_ProjectAsTagBtn.Size = new System.Drawing.Size(49, 17);
@@ -96,6 +102,7 @@
 			// 
 			this.m_ProjectAsParentTaskBtn.AutoSize = true;
 			this.m_ProjectAsParentTaskBtn.Checked = true;
+			this.m_ProjectAsParentTaskBtn.ForeColor = System.Drawing.SystemColors.WindowText;
 			this.m_ProjectAsParentTaskBtn.Location = new System.Drawing.Point(14, 65);
 			this.m_ProjectAsParentTaskBtn.Name = "m_ProjectAsParentTaskBtn";
 			this.m_ProjectAsParentTaskBtn.Size = new System.Drawing.Size(88, 17);
@@ -108,7 +115,7 @@
 			// 
 			this.m_OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.m_OK.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.m_OK.Location = new System.Drawing.Point(124, 184);
+			this.m_OK.Location = new System.Drawing.Point(154, 219);
 			this.m_OK.Name = "m_OK";
 			this.m_OK.Size = new System.Drawing.Size(75, 23);
 			this.m_OK.TabIndex = 2;
@@ -119,7 +126,7 @@
 			// 
 			this.m_Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.m_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.m_Cancel.Location = new System.Drawing.Point(205, 184);
+			this.m_Cancel.Location = new System.Drawing.Point(236, 219);
 			this.m_Cancel.Name = "m_Cancel";
 			this.m_Cancel.Size = new System.Drawing.Size(75, 23);
 			this.m_Cancel.TabIndex = 2;
@@ -130,7 +137,8 @@
 			// 
 			this.m_ContextGroup.Controls.Add(this.m_ContextAsCategoryBtn);
 			this.m_ContextGroup.Controls.Add(this.m_ContextAsTagBtn);
-			this.m_ContextGroup.Location = new System.Drawing.Point(8, 107);
+			this.m_ContextGroup.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.m_ContextGroup.Location = new System.Drawing.Point(12, 112);
 			this.m_ContextGroup.Name = "m_ContextGroup";
 			this.m_ContextGroup.Size = new System.Drawing.Size(271, 69);
 			this.m_ContextGroup.TabIndex = 3;
@@ -142,35 +150,50 @@
 			this.m_ProjectGroup.Controls.Add(this.m_ProjectAsCategoryBtn);
 			this.m_ProjectGroup.Controls.Add(this.m_ProjectAsTagBtn);
 			this.m_ProjectGroup.Controls.Add(this.m_ProjectAsParentTaskBtn);
-			this.m_ProjectGroup.Location = new System.Drawing.Point(8, 8);
+			this.m_ProjectGroup.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.m_ProjectGroup.Location = new System.Drawing.Point(13, 8);
 			this.m_ProjectGroup.Name = "m_ProjectGroup";
 			this.m_ProjectGroup.Size = new System.Drawing.Size(271, 93);
 			this.m_ProjectGroup.TabIndex = 4;
 			this.m_ProjectGroup.TabStop = false;
 			this.m_ProjectGroup.Text = "Import \'Projects\' as";
 			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.Window;
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.m_ProjectGroup);
+			this.panel1.Controls.Add(this.m_ContextGroup);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(12, 13);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(298, 195);
+			this.panel1.TabIndex = 5;
+			// 
 			// TodoTxtImporterOptionsForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.m_Cancel;
-			this.ClientSize = new System.Drawing.Size(285, 215);
+			this.ClientSize = new System.Drawing.Size(322, 250);
 			this.Controls.Add(this.m_Cancel);
 			this.Controls.Add(this.m_OK);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.m_ContextGroup);
-			this.Controls.Add(this.m_ProjectGroup);
-			this.Icon = Properties.Resources.TodoTxt;
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = global::TodoTxtImpExp.Properties.Resources.TodoTxt;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "TodoTxtImporterOptionsForm";
 			this.ShowInTaskbar = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "todo.txt Options";
+			this.Text = "todo.txt Import Options";
 			this.m_ContextGroup.ResumeLayout(false);
 			this.m_ContextGroup.PerformLayout();
 			this.m_ProjectGroup.ResumeLayout(false);
 			this.m_ProjectGroup.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -186,5 +209,6 @@
 		private System.Windows.Forms.Button m_Cancel;
 		private System.Windows.Forms.GroupBox m_ContextGroup;
 		private System.Windows.Forms.GroupBox m_ProjectGroup;
+		private System.Windows.Forms.Panel panel1;
 	}
 }
