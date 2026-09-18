0acac541945f81fd0f6de120ebae930ddf0a53d9
abstractspoon <abstractspoon@users.noreply.github.com>
2026-09-18 13:19:59 +1000

    Fix comparison typo

--- Plugins/UIExtension/WordCloudUIExtension/WordCloudUIExtensionCore/TaskMatchesListView.cs
+++ Plugins/UIExtension/WordCloudUIExtension/WordCloudUIExtensionCore/TaskMatchesListView.cs
@@ -61,17 +61,17 @@ namespace WordCloudUIExtension
 
 		private void RefreshIDColumnWidth()
 		{
-			if (Columns.Count > 2)
-			{
-				using (var graphics = CreateGraphics())
-				{
-					int headerPadding = (int)(graphics.MeasureString("o", Font).Width); // Fudge
-					int headerWidth = (int)(graphics.MeasureString(Columns[1].Text, Font).Width + (headerPadding * 2));
-					int maxItemWidth = (int)(graphics.MeasureString(m_MaxTaskId.ToString(), Font).Width + (2 * DPIScaling.Scale(2)));
+			if (Columns.Count < 2)
+				return;
 
-					Columns[1].Width = Math.Max(headerWidth, maxItemWidth);
-					Columns[0].Width = (ClientRectangle.Width - Columns[1].Width - 2);
-				}
+			using (var graphics = CreateGraphics())
+			{
+				int headerPadding = (int)(graphics.MeasureString("o", Font).Width); // Fudge
+				int headerWidth = (int)(graphics.MeasureString(Columns[1].Text, Font).Width + (headerPadding * 2));
+				int maxItemWidth = (int)(graphics.MeasureString(m_MaxTaskId.ToString(), Font).Width + (2 * DPIScaling.Scale(2)));
+
+				Columns[1].Width = Math.Max(headerWidth, maxItemWidth);
+				Columns[0].Width = (ClientRectangle.Width - Columns[1].Width - 2);
 			}
 		}
 
