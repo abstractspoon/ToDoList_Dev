d63b11f015c5280aa934cc3f04b6dce8525170f4
abstractspoon <abstractspoon@users.noreply.github.com>
2026-09-23 13:39:13 +1000

    Fix 'Task List View' row background colour filling

--- Plugins/PluginHelpers/TaskListView.cpp
+++ Plugins/PluginHelpers/TaskListView.cpp
@@ -933,7 +933,7 @@ void TaskListView::OnDrawItem(DrawListViewItemEventArgs^ e)
 	auto backColor = GetBackColor(task, e->Item->Index);
 	auto itemRect = e->Item->Bounds;
 
-	e->Graphics->FillRectangle(gcnew SolidBrush(backColor), 0, itemRect.Top + 1, Width, itemRect.Height - 1);
+	e->Graphics->FillRectangle(gcnew SolidBrush(backColor), 0, itemRect.Top, Width, itemRect.Height);
 
 	// Horizontal grid line full width
 	Pen^ gridPen = nullptr;
